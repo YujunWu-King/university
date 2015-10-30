7c5b812729fb3d960a83388fc89d8633fb85f79b
石华 <shihua@tranzvision.com.cn>
2015-10-30 14:32:16 +0800

    增加update方法

--- src/main/java/com/tranzvision/gd/util/sql/SqlQuery.java
+++ src/main/java/com/tranzvision/gd/util/sql/SqlQuery.java
@@ -57,46 +57,46 @@ public class SqlQuery {
 		} catch (DataAccessException e) {
 			System.out.println(e.getMessage());
 		}
-		
+
 		return null;
 	}
 
 	@SuppressWarnings("unchecked")
 	public <T> List<T> queryForList(String sql) {
-		
+
 		try {
 			return (List<T>) jdbcTemplate.queryForList(sql);
 		} catch (DataAccessException e) {
 			System.out.println(e.getMessage());
 		}
-		
+
 		return null;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public <T> List<T> queryForList(String sql,Object[] args) {
-		
+	public <T> List<T> queryForList(String sql, Object[] args) {
+
 		try {
 			return (List<T>) jdbcTemplate.queryForList(sql, args);
 		} catch (DataAccessException e) {
 			System.out.println(e.getMessage());
 		}
-		
+
 		return null;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public <T> List<T> queryForList(String sql,String elmType) {
-		
+	public <T> List<T> queryForList(String sql, String elmType) {
+
 		try {
 			return (List<T>) jdbcTemplate.queryForList(sql, varType2Class.change2Class(elmType));
 		} catch (DataAccessException e) {
 			System.out.println(e.getMessage());
 		}
-		
+
 		return null;
 	}
-	
+
 	public Map<String, Object> queryForMap(String sql) {
 
 		try {
@@ -109,11 +109,11 @@ public class SqlQuery {
 
 		return null;
 	}
-	
+
 	public Map<String, Object> queryForMap(String sql, Object[] args) {
 
 		try {
-			return jdbcTemplate.queryForMap(sql,args);
+			return jdbcTemplate.queryForMap(sql, args);
 		} catch (IncorrectResultSizeDataAccessException ex) {
 			System.out.println(ex.getMessage());
 		} catch (DataAccessException e) {
@@ -121,6 +121,24 @@ public class SqlQuery {
 		}
 
 		return null;
+	}
+
+	public int update(String sql) {
+		try {
+			return jdbcTemplate.update(sql);
+		} catch (DataAccessException e) {
+			System.out.println(e.getMessage());
+		}
+		return -1;
+	}
+
+	public int update(String sql, Object[] args) {
+		try {
+			return jdbcTemplate.update(sql, args);
+		} catch (DataAccessException e) {
+			System.out.println(e.getMessage());
+		}
+		return -1;
 	}
 
 }
