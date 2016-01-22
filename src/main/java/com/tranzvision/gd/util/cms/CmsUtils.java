b0e22b4790b96b6f39b058441a035776be707aaa
唐敏敏 <tangmm@tranzvision.com.cn>
2016-01-22 15:07:53 +0800

    tangmm 20160122 内容模板预览修改

--- src/main/java/com/tranzvision/gd/util/cms/CmsUtils.java
+++ src/main/java/com/tranzvision/gd/util/cms/CmsUtils.java
@@ -9,7 +9,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import com.tranzvision.gd.util.base.GetSpringBeanUtil;
 import com.tranzvision.gd.util.cms.action.directive.ArticleAttachmentDirective;
 import com.tranzvision.gd.util.cms.action.directive.ArticleImageDirective;
 import com.tranzvision.gd.util.cms.action.directive.ContentDirective;
@@ -28,7 +30,7 @@ import com.tranzvision.gd.util.cms.manager.main.ChannelMngImpl;
 public class CmsUtils {
 	// 解析文章内容；
 	// 解析文章内容；
-	public String content(String siteId, String chnlId, String id) {
+	public String content(String siteId, String chnlId, String id,String contentPath) {
 
 		if (StringUtils.isBlank(id)) {
 			return "文章编号为空";
@@ -44,7 +46,17 @@ public class CmsUtils {
 		if (channel == null) {
 			return "未找到对应的栏目";
 		}
-
+		
+		String jgId = "";
+		try{
+			GetSpringBeanUtil getSpringBeanUtil = new GetSpringBeanUtil();
+			JdbcTemplate jdbcTemplate = (JdbcTemplate) getSpringBeanUtil.getSpringBeanByID("jdbcTemplate");
+			String sql = "select TZ_JG_ID from PS_TZ_SITEI_DEFN_T where TZ_SITEI_ID=?";
+			jgId = jdbcTemplate.queryForObject(sql,new Object[]{siteId},String.class);
+		}catch(Exception e){
+			jgId = "";
+		}
+		
 		Map<String, Object> root = new HashMap<String, Object>();
 		root.put("CmsContent", new ContentDirective());
 		root.put("text_cut", new TextCutDirective());
@@ -53,6 +65,14 @@ public class CmsUtils {
 		root.put(CHANNEL, channel);
 		root.put(ART_ID, id);
 		root.put(SITE_ID, siteId);
+		root.put("TzUniversityContextPath", contentPath);
+		
+		String jsAndCss = "<link href=\""+contentPath+"/statics/css/website/orgs/"+jgId.toLowerCase()+"/"+siteId+"/style_"+jgId.toLowerCase()+".css\" rel=\"stylesheet\" type=\"text/css\" />"
+				+ "<script type=\"text/javascript\" src=\""+contentPath+"/statics/js/lib/jquery/jquery.min.js\"></script>"
+				+ "<script type=\"text/javascript\" src=\""+contentPath+"/statics/js/tranzvision/extjs/app/view/website/set/js/pagefunc.js\"></script>"
+				+ "<script type=\"text/javascript\" src=\""+contentPath+"/statics/js/tranzvision/extjs/app/view/website/set/js/pic_list.js\"></script>";
+
+		root.put("scriptsAndcss", jsAndCss);
 
 		/* 获得模版内容 */
 		String tplSource = tpl.getPcContent();
@@ -63,7 +83,11 @@ public class CmsUtils {
 
 		StringWriter out = new StringWriter();
 		FreeMarkertUtils.processTemplate(tplSource, tplName, root, out);
-
-		return out.toString();
+		
+		
+		
+		String contents = out.toString();
+		
+		return contents;
 	}
 }
