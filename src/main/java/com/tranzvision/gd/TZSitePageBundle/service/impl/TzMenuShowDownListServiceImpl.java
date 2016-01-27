e95c48b9e028139e15a05b4e89bc5d6c26df812a
石华 <shihua@tranzvision.com.cn>
2016-01-28 00:37:20 +0800

    增加菜单名称替换

--- src/main/java/com/tranzvision/gd/TZSitePageBundle/service/impl/TzMenuShowDownListServiceImpl.java
+++ src/main/java/com/tranzvision/gd/TZSitePageBundle/service/impl/TzMenuShowDownListServiceImpl.java
@@ -64,10 +64,14 @@ public class TzMenuShowDownListServiceImpl extends FrameworkImpl {
 					if (mapSiteOrg != null) {
 						orgid = mapSiteOrg.get("TZ_JG_ID") == null ? "" : String.valueOf(mapSiteOrg.get("TZ_JG_ID"));
 					}
+					
+					sql = "select TZ_MENU_NAME from PS_TZ_SITEI_MENU_T where TZ_SITEI_ID=? and TZ_MENU_ID=? and TZ_MENU_STATE='Y'";
+					String strMenuName = sqlQuery.queryForObject(sql, new Object[] { strSiteId, strMenuId }, "String");
 
 					strResultContent = siteRepCssServiceImpl.repContextPath(strResultContent);
 					strResultContent = siteRepCssServiceImpl.repTitle(strResultContent, strSiteId);
 					strResultContent = siteRepCssServiceImpl.repCss(strResultContent, strSiteId);
+					strResultContent = siteRepCssServiceImpl.repMenuName(strResultContent, strMenuName);
 
 					String strSelfJavascripts = tzGDObject.getHTMLText("HTML.TZSitePageBundle.TzScriptsDownload",
 							ctxPath);
