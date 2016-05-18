45de682125d66f34d34928e0cb50c3ac77a371e3
wangruili <wangrl@tranzvision.com.cn>
2016-05-18 16:51:10 +0800

    Bug3263 日历选择框没有汉化

--- src/main/webapp/statics/js/lib/timepicker-addon/jquery-ui-timepicker-addon.js
+++ src/main/webapp/statics/js/lib/timepicker-addon/jquery-ui-timepicker-addon.js
@@ -43,6 +43,8 @@
 			millisecText: '毫秒',
 			microsecText: '微秒',
 			timezoneText: '时区',
+            dayNamesMin: ['日','一','二','三','四','五','六'], 
+            monthNamesShort: ['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'],
 			isRTL: false
 		};
 		this.regional['ENG'] = { // ENG regional settings
