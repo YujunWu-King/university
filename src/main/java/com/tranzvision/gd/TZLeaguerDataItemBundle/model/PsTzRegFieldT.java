4b4619f4379f26ff074e394e99a1eca4d1c517ab
唐敏敏 <tangmm@tranzvision.com.cn>
2016-08-24 10:05:41 +0800

    20160824 多站点

---
+++ src/main/java/com/tranzvision/gd/TZLeaguerDataItemBundle/model/PsTzRegFieldT.java
@@ -0,0 +1,113 @@
+package com.tranzvision.gd.TZLeaguerDataItemBundle.model;
+
+public class PsTzRegFieldT extends PsTzRegFieldTKey {
+    private String tzJgId;
+
+    private String tzRedFldYsmc;
+
+    private String tzRegFieldName;
+
+    private Integer tzOrder;
+
+    private String tzEnable;
+
+    private String tzIsRequired;
+
+    private String tzSysfieldFlag;
+
+    private String tzFieldType;
+
+    private String tzDefVal;
+
+    private String tzYxbgSrklx;
+
+    private String tzIsShowwzsy;
+
+    public String getTzJgId() {
+        return tzJgId;
+    }
+
+    public void setTzJgId(String tzJgId) {
+        this.tzJgId = tzJgId == null ? null : tzJgId.trim();
+    }
+
+    public String getTzRedFldYsmc() {
+        return tzRedFldYsmc;
+    }
+
+    public void setTzRedFldYsmc(String tzRedFldYsmc) {
+        this.tzRedFldYsmc = tzRedFldYsmc == null ? null : tzRedFldYsmc.trim();
+    }
+
+    public String getTzRegFieldName() {
+        return tzRegFieldName;
+    }
+
+    public void setTzRegFieldName(String tzRegFieldName) {
+        this.tzRegFieldName = tzRegFieldName == null ? null : tzRegFieldName.trim();
+    }
+
+    public Integer getTzOrder() {
+        return tzOrder;
+    }
+
+    public void setTzOrder(Integer tzOrder) {
+        this.tzOrder = tzOrder;
+    }
+
+    public String getTzEnable() {
+        return tzEnable;
+    }
+
+    public void setTzEnable(String tzEnable) {
+        this.tzEnable = tzEnable == null ? null : tzEnable.trim();
+    }
+
+    public String getTzIsRequired() {
+        return tzIsRequired;
+    }
+
+    public void setTzIsRequired(String tzIsRequired) {
+        this.tzIsRequired = tzIsRequired == null ? null : tzIsRequired.trim();
+    }
+
+    public String getTzSysfieldFlag() {
+        return tzSysfieldFlag;
+    }
+
+    public void setTzSysfieldFlag(String tzSysfieldFlag) {
+        this.tzSysfieldFlag = tzSysfieldFlag == null ? null : tzSysfieldFlag.trim();
+    }
+
+    public String getTzFieldType() {
+        return tzFieldType;
+    }
+
+    public void setTzFieldType(String tzFieldType) {
+        this.tzFieldType = tzFieldType == null ? null : tzFieldType.trim();
+    }
+
+    public String getTzDefVal() {
+        return tzDefVal;
+    }
+
+    public void setTzDefVal(String tzDefVal) {
+        this.tzDefVal = tzDefVal == null ? null : tzDefVal.trim();
+    }
+
+    public String getTzYxbgSrklx() {
+        return tzYxbgSrklx;
+    }
+
+    public void setTzYxbgSrklx(String tzYxbgSrklx) {
+        this.tzYxbgSrklx = tzYxbgSrklx == null ? null : tzYxbgSrklx.trim();
+    }
+
+    public String getTzIsShowwzsy() {
+        return tzIsShowwzsy;
+    }
+
+    public void setTzIsShowwzsy(String tzIsShowwzsy) {
+        this.tzIsShowwzsy = tzIsShowwzsy == null ? null : tzIsShowwzsy.trim();
+    }
+}
