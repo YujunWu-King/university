f707ebc14be6ac73233824c1c8a04a15ab9f5fe5
叶少威 <yesw@tranzvision.com.cn>
2017-01-17 18:00:16 +0800

    菜单导航页图标风格（新增和调整主题风格）
    Signed-off-by: 叶少威 <yesw@tranzvision.com.cn>

--- src/main/webapp/statics/js/tranzvision/extjs/app/view/thumbnails/Thumbnails.js
+++ src/main/webapp/statics/js/tranzvision/extjs/app/view/thumbnails/Thumbnails.js
@@ -9,15 +9,15 @@
 
     initComponent: function() {
         var backgrounds = {
-            tranzvision: ['Sencha-Examples','square'],
-            maple: ['Sencha-Examples','square'],
+            tranzvision: ['Sencha-Examples','icon-square'],
+            maple: ['Sencha-Examples','icon-square'],
             simple:['FontAwesome','fa-rounded-square']
         };
 
         this.tpl =
             '<tpl for=".">' +
                 '<div class="thumbnail-item">' +
-                '<div class="thumbnail-icon-wrap icon-' + backgrounds[Ext.themeName][1] +
+                '<div class="thumbnail-icon-wrap ' + backgrounds[Ext.themeName][1] +
                 '" style="font-family:'+backgrounds[Ext.themeName][0]+'">' +
                 '<div class="thumbnail-icon {iconCls}"></div>' +
                 '</div>' +
