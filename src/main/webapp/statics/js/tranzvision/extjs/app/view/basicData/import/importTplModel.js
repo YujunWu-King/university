83dc0c8f19da6dd549b5e0fef8c936b76fc77fa1
叶少威 <yesw@tranzvision.com.cn>
2017-01-22 14:41:32 +0800

    导入导出配置开发

---
+++ src/main/webapp/statics/js/tranzvision/extjs/app/view/basicData/import/importTplModel.js
@@ -0,0 +1,10 @@
+Ext.define('KitchenSink.view.basicData.resData.import.importTplModel', {
+    extend: 'Ext.data.Model',
+    fields: [
+        {name: 'tplId'},
+        {name: 'tplName'},
+        {name: 'appClassName'},
+        {name: 'appClassPath'},
+        {name: 'appClassMehtod'}
+	]
+});
