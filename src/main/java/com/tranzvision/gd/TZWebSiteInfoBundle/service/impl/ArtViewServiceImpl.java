05f99b88934890ceeac47ea5dbe48f6f6743407e
mabc <mabc@tranzvision.com.cn>
2017-02-09 23:24:29 +0800

    1.活动管理添加听众 2.内容管理和活动管理发布的动态链接，添加听众控制

--- src/main/java/com/tranzvision/gd/TZWebSiteInfoBundle/service/impl/ArtViewServiceImpl.java
+++ src/main/java/com/tranzvision/gd/TZWebSiteInfoBundle/service/impl/ArtViewServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.tranzvision.gd.TZBaseBundle.service.impl.FrameworkImpl;
 import com.tranzvision.gd.util.sql.SqlQuery;
+import com.tranzvision.gd.TZAuthBundle.service.impl.TzLoginServiceImpl;
 
 /**
  * 查看发布内容；原：TZ_GD_ARTGL:ArtView
@@ -22,6 +23,8 @@ public class ArtViewServiceImpl extends FrameworkImpl {
 	private SqlQuery jdbcTemplate;
 	@Autowired
 	private HttpServletRequest request;
+	@Autowired
+	private TzLoginServiceImpl tzLoginServiceImpl;
 	
 	@Override
 	public String tzGetHtmlContent(String strParams) {
@@ -31,34 +34,66 @@ public class ArtViewServiceImpl extends FrameworkImpl {
 		String siteId = request.getParameter("siteId");
 		String columnId = request.getParameter("columnId");
 		String artId = request.getParameter("artId");
+		
+		String oprid = tzLoginServiceImpl.getLoginedManagerOprid(request);
+		
 		if(siteId != null && !"".equals(siteId)
 				&& columnId != null && !"".equals(columnId)
 				&& artId != null && !"".equals(artId)){
-			//查看是否是外部链接;
-			String sql = "SELECT TZ_ART_TYPE1,TZ_OUT_ART_URL FROM PS_TZ_ART_REC_TBL WHERE TZ_ART_ID = ?";
-			Map< String, Object> map = jdbcTemplate.queryForMap(sql,new Object[]{artId});
-			if(map != null){
-				String artType = (String) map.get("TZ_ART_TYPE1");
-				if("B".equals(artType)){
-					String outurl = (String) map.get("TZ_OUT_ART_URL");
-					if(outurl == null || "".equals(outurl)){
-						strRet = "未定义外部链接";
-					}else{
-						strRet = "<script type=\"text/javascript\">;location.href=\""+outurl+"\"</script>";
-					}
+			
+			//检查是否有听众控制Mabc2019.02.09
+			String AudError="N";
+			String sqlAud = "select C.TZ_PROJECT_LIMIT,'Y' AUDFLG from PS_TZ_ART_AUDIENCE_T A,PS_TZ_AUD_LIST_T B,PS_TZ_ART_REC_TBL C WHERE B.TZ_DXZT='A' AND A.TZ_AUD_ID=B.TZ_AUD_ID AND A.TZ_ART_ID=C.TZ_ART_ID AND B.OPRID=? AND A.TZ_ART_ID=? LIMIT 1";
+			Map< String, Object> mapAud = jdbcTemplate.queryForMap(sqlAud,new Object[]{oprid,artId});
+			if (mapAud!=null){
+				
+				//发布对象
+				String pubFlg = (String) mapAud.get("TZ_PROJECT_LIMIT");
+				//是否当前人存在于该听众
+				String audFlg = (String) mapAud.get("AUDFLG");
+				
+				if (pubFlg=="B"&&audFlg!="Y"){
+					//如果设置发布对象为听众，不在听众中，不能访问
+					strRet = " 您无权限查看";
+					AudError="Y";
 				}else{
-					String htmlSQL = "select TZ_ART_NEWS_DT,TZ_ART_CONENT_SCR from PS_TZ_LM_NR_GL_T where TZ_SITE_ID=? and TZ_COLU_ID=? and TZ_ART_ID=? and TZ_ART_NEWS_DT <= now()";
-					Map< String, Object> contentMap = jdbcTemplate.queryForMap(htmlSQL,new Object[]{siteId,columnId,artId});
-					if(contentMap == null){
-						strRet = "当前时间不可查看该内容";
-					}else{
-						strRet = (String) contentMap.get("TZ_ART_CONENT_SCR");
-					}
+					AudError="N";
 				}
 				
 			}else{
-				strRet = "参数错误，请联系系统管理员";
+				
+				AudError="N";
 			}
+			
+			if (AudError=="N"){
+			
+				//查看是否是外部链接;
+				String sql = "SELECT TZ_ART_TYPE1,TZ_OUT_ART_URL FROM PS_TZ_ART_REC_TBL WHERE TZ_ART_ID = ?";
+				Map< String, Object> map = jdbcTemplate.queryForMap(sql,new Object[]{artId});
+				if(map != null){
+					String artType = (String) map.get("TZ_ART_TYPE1");
+					if("B".equals(artType)){
+						String outurl = (String) map.get("TZ_OUT_ART_URL");
+						if(outurl == null || "".equals(outurl)){
+							strRet = "未定义外部链接";
+						}else{
+							strRet = "<script type=\"text/javascript\">;location.href=\""+outurl+"\"</script>";
+						}
+					}else{
+						String htmlSQL = "select TZ_ART_NEWS_DT,TZ_ART_CONENT_SCR from PS_TZ_LM_NR_GL_T where TZ_SITE_ID=? and TZ_COLU_ID=? and TZ_ART_ID=? and TZ_ART_NEWS_DT <= now()";
+						Map< String, Object> contentMap = jdbcTemplate.queryForMap(htmlSQL,new Object[]{siteId,columnId,artId});
+						if(contentMap == null){
+							strRet = "当前时间不可查看该内容";
+						}else{
+							strRet = (String) contentMap.get("TZ_ART_CONENT_SCR");
+						}
+					}
+					
+				}else{
+					strRet = "参数错误，请联系系统管理员";
+				}
+			}
+			
 		}
 		
 		return strRet;
