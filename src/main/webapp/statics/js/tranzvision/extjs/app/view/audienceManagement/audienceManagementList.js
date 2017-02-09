33b4ad741178066653d09c8732c642dd0fd9e74c
顾贤达 <guxd@tranzvision.com.cn>
2017-02-09 10:23:28 +0800

    听众管理提交

--- src/main/webapp/statics/js/tranzvision/extjs/app/view/audienceManagement/audienceManagementList.js
+++ src/main/webapp/statics/js/tranzvision/extjs/app/view/audienceManagement/audienceManagementList.js
@@ -1,5 +1,6 @@
 Ext.define('KitchenSink.view.audienceManagement.audienceManagementList', {
     extend: 'Ext.grid.Panel',
+  //  extend: 'Ext.panel.Panel',
 	requires: [
         'Ext.data.*',
         'Ext.grid.*',
@@ -8,7 +9,9 @@ Ext.define('KitchenSink.view.audienceManagement.audienceManagementList', {
         'Ext.ux.ProgressBarPager',
 		'KitchenSink.view.audienceManagement.audienceManagementModel',
         'KitchenSink.view.audienceManagement.audienceManagementStore',
+        'KitchenSink.view.audienceManagement.newAudWindowStore',
 		'KitchenSink.view.audienceManagement.audienceManagementController'
+		
     ],
     xtype: 'audienceManagementDa',
 	controller: 'audienceManagementController',
@@ -17,7 +20,7 @@ Ext.define('KitchenSink.view.audienceManagement.audienceManagementList', {
         type: 'checkboxmodel'
     },
 	style:"margin:8px",
-    multiSelect: true,
+ //   multiSelect: true,
     title: '听众管理系统',
     viewConfig: {
         enableTextSelection: true
@@ -36,34 +39,87 @@ Ext.define('KitchenSink.view.audienceManagement.audienceManagementList', {
 		xtype:"toolbar",
 		items:[
 			{text:"查询",tooltip:"查询数据",iconCls: "query",handler:"searchComList"},"-",
-			{text:"新增",tooltip:"新增数据",iconCls:"add",handler:"addComRegInfo"},"-",
-			{text:"编辑",tooltip:"编辑数据",iconCls:"edit",handler:"editComRegInfo"},"-",
+		//	{text:"新增",tooltip:"新增数据",iconCls:"add",handler:"addComRegInfo"},"-",
+			{text:"新增",tooltip:"新增数据",iconCls:"add",handler:"addPageRegInfo"},"-",
+			{text:"编辑",tooltip:"编辑数据",iconCls:"edit",handler:"editPageRegInfo"},"-",
 			{text:"删除",tooltip:"删除选中的数据",iconCls:"remove",handler:"deleteComRegInfos"}
 		]
 	}],
     initComponent: function () { 
 	    //组件注册信息列表
+    	
+    	
 	    var store = new KitchenSink.view.audienceManagement.audienceManagementStore();
+	    
+	    var orgid = Ext.tzOrgID;
+	    
+	//    console.log(orgid);
+	//    var store = new KitchenSink.view.audienceManagement.newAudWindowStore();
         Ext.apply(this, {
-            columns: [{ 
-                text: '组件ID',
-                dataIndex: 'comID',
-				width: 240
-            },{
+            columns: [
+                      
+            { 
+                text: '听众ID',
+                hidden:true,
+                dataIndex: 'audId',
+				Width: 400,
+				flex:1
+            },{ 
+                text: '听众描述',
+                hidden:true,
+                dataIndex: 'audMS',
+				Width: 400,
+				flex:1
+            },{ 
+                text: '听众SQL',
+                hidden:true,
+                dataIndex: 'audSQL',
+				Width: 400,
+				flex:1
+            },{ 
                 text: '听众名称',
-                sortable: true,
-                dataIndex: 'comName',
-                minWidth: 100,
+                dataIndex: 'audName',
+				Width: 400,
 				flex:1
             },{
+                text: '听众类型',
+                sortable: true,
+                dataIndex: 'audType',
+                minWidth: 260,
+                renderer:function(v) {
+           		 if (v == '1') {
+           			 return "动态听众";
+           		 } else if (v == '2') {
+           			 return "静态听众";
+           		 }
+           	 }
+				//flex:1
+            },
+            {
+                text: '有效状态',
+                sortable: true,
+                dataIndex: 'audStat',
+                minWidth: 260,
+                renderer:function(v) {
+              		 if (v == '1') {
+              			 return "有效";
+              		 } else if (v == '2') {
+              			 return "无效";
+              		 }
+              	 }
+				//flex:1
+            },
+            
+            {
                menuDisabled: true,
                sortable: false,
 			   width:60,
                align:'center',
                xtype: 'actioncolumn',
 			   items:[
-				  {iconCls: 'edit',tooltip: '编辑',handler: 'editSelComRegInfo'},
-			   	  {iconCls: 'remove',tooltip: '删除',handler: 'deleteComRegInfo'}
+				//  {iconCls: 'edit',tooltip: '编辑',handler: 'editSelComRegInfo'},
+				  {iconCls: 'edit',tooltip: '编辑',handler: 'editPageRegInfoOne'},
+			   	//  {iconCls: 'remove',tooltip: '删除',handler: 'deleteComRegInfo'}
 			   ]
             }],
 			store: store,
