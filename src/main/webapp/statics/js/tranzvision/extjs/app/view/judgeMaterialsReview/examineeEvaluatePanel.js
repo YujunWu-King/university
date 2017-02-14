3b21d1098f969752aa0e4d9b521421fc5b262c81
luyan <luyan@tranzvision.com.cn>
2017-02-14 15:05:31 +0800

    MBA材料评审打分页评委端

--- src/main/webapp/statics/js/tranzvision/extjs/app/view/judgeMaterialsReview/examineeEvaluatePanel.js
+++ src/main/webapp/statics/js/tranzvision/extjs/app/view/judgeMaterialsReview/examineeEvaluatePanel.js
@@ -8,6 +8,7 @@ Ext.define('KitchenSink.view.judgeMaterialsReview.examineeEvaluatePanel',{
 		'Ext.layout.container.Border',
 		'Ext.toolbar.Paging',
 		'Ext.ux.ProgressBarPager',
+		'tranzvision.extension.grid.column.Link',
 		'KitchenSink.view.judgeMaterialsReview.examineeEvaluateController'
 	],
 	layout:'fit',
@@ -68,7 +69,7 @@ Ext.define('KitchenSink.view.judgeMaterialsReview.examineeEvaluatePanel',{
 							text: '排名',
                             dataIndex: 'examineeRank',
                             width: 50
-						}，{
+						},{
 							text:'总分',
 							dataIndex:'examineeTotalScore',
 							width:50
