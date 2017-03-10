5d60463b8da3ff72b23345e39cdde54954f61b84
wangruili <wangrl@tranzvision.com.cn>
2017-03-10 20:13:37 +0800

    奖学金管理--申请结果表添加备注字段

--- src/main/java/com/tranzvision/gd/TZSchlrBundle/model/PsTzSchlrRsltTbl.java
+++ src/main/java/com/tranzvision/gd/TZSchlrBundle/model/PsTzSchlrRsltTbl.java
@@ -3,11 +3,21 @@ package com.tranzvision.gd.TZSchlrBundle.model;
 public class PsTzSchlrRsltTbl extends PsTzSchlrRsltTblKey {
     private String tzIsApply;
 
+    private String tzNote;
+
     public String getTzIsApply() {
         return tzIsApply;
     }
 
     public void setTzIsApply(String tzIsApply) {
         this.tzIsApply = tzIsApply == null ? null : tzIsApply.trim();
+    }
+
+    public String getTzNote() {
+        return tzNote;
+    }
+
+    public void setTzNote(String tzNote) {
+        this.tzNote = tzNote == null ? null : tzNote.trim();
     }
 }
