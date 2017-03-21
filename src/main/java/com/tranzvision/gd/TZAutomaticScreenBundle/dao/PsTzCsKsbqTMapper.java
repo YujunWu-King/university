b19d17e9e8d15d4d2b7e73dc99c285e8689f4ae5
yantt <yantt@tranzvision.com.cn>
2017-03-21 10:30:33 +0800

    自动标签考生表增加标签名称 燕婷婷

--- src/main/java/com/tranzvision/gd/TZAutomaticScreenBundle/dao/PsTzCsKsbqTMapper.java
+++ src/main/java/com/tranzvision/gd/TZAutomaticScreenBundle/dao/PsTzCsKsbqTMapper.java
@@ -1,11 +1,18 @@
 package com.tranzvision.gd.TZAutomaticScreenBundle.dao;
 
+import com.tranzvision.gd.TZAutomaticScreenBundle.model.PsTzCsKsbqT;
 import com.tranzvision.gd.TZAutomaticScreenBundle.model.PsTzCsKsbqTKey;
 
 public interface PsTzCsKsbqTMapper {
     int deleteByPrimaryKey(PsTzCsKsbqTKey key);
 
-    int insert(PsTzCsKsbqTKey record);
+    int insert(PsTzCsKsbqT record);
 
-    int insertSelective(PsTzCsKsbqTKey record);
+    int insertSelective(PsTzCsKsbqT record);
+
+    PsTzCsKsbqT selectByPrimaryKey(PsTzCsKsbqTKey key);
+
+    int updateByPrimaryKeySelective(PsTzCsKsbqT record);
+
+    int updateByPrimaryKey(PsTzCsKsbqT record);
 }
