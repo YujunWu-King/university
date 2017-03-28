68693d838074e0f15d451737f8b94472586c71d0
yantt <yantt@tranzvision.com.cn>
2017-03-28 20:28:16 +0800

    微信签名 晒录取通知书

--- src/main/java/com/tranzvision/gd/TZApplicationCenterBundle/service/impl/CertGetParameter.java
+++ src/main/java/com/tranzvision/gd/TZApplicationCenterBundle/service/impl/CertGetParameter.java
@@ -1,130 +1,243 @@
 package com.tranzvision.gd.TZApplicationCenterBundle.service.impl;
 
-import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.tranzvision.gd.TZWeChatBundle.service.impl.TzWeChartJSSDKSign;
+import javax.servlet.http.HttpServletRequest;
+import com.tranzvision.gd.util.base.GetSpringBeanUtil;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
-import com.tranzvision.gd.util.base.GetSpringBeanUtil;
-import com.tranzvision.gd.util.encrypt.DESUtil;
-
 /**
-
-* @author YTT
-* @Time 2017-2-14
-* @paramtersm 机构id,站点id,申请人oprid,报名表编号
-* @return 晒录取通知书证书模板中的系统变量解析值
-
-*/
+ * 
+ * @author YTT
+ * @Time 2017-2-14
+ * @paramtersm 机构id,站点id,申请人oprid,报名表编号
+ * @return 晒录取通知书证书模板中的系统变量解析值
+ * 
+ */
 public class CertGetParameter {
 
-	
 	// 清华MBA录取通知书-分享缩略图;
 	public String getCertLogo(String[] paramters) {
 		try {
 			GetSpringBeanUtil getSpringBeanUtil = new GetSpringBeanUtil();
 			JdbcTemplate jdbcTemplate = (JdbcTemplate) getSpringBeanUtil.getSpringBeanByID("jdbcTemplate");
-			String CertLogoSql="SELECT CONCAT(A.TZ_ATT_A_URL,A.TZ_ATTACHSYSFILENA) FROM PS_TZ_CERTIMAGE_TBL A,PS_TZ_CERTTMPL_TBL B WHERE A.TZ_ATTACHSYSFILENA=B.TZ_ATTACHSYSFILENA AND B.TZ_JG_ID=? AND B.TZ_CERT_TMPL_ID=(SELECT B.TZ_CERT_TMPL_ID FROM PS_TZ_APP_INS_T A,PS_TZ_PRJ_INF_T B WHERE A.TZ_APP_INS_ID=? AND A.TZ_APP_TPL_ID=B.TZ_APP_MODAL_ID)";
-			
-			String jgId=paramters[0];
-			String siteId=paramters[1];
-			String oprid=paramters[2];
-			String appIns=paramters[3];
+			String CertLogoSql = "SELECT CONCAT(A.TZ_ATT_A_URL,A.TZ_ATTACHSYSFILENA) FROM PS_TZ_CERTIMAGE_TBL A,PS_TZ_CERTTMPL_TBL B WHERE A.TZ_ATTACHSYSFILENA=B.TZ_ATTACHSYSFILENA AND B.TZ_JG_ID=? AND B.TZ_CERT_TMPL_ID=(SELECT B.TZ_CERT_TMPL_ID FROM PS_TZ_APP_INS_T A,PS_TZ_PRJ_INF_T B WHERE A.TZ_APP_INS_ID=? AND A.TZ_APP_TPL_ID=B.TZ_APP_MODAL_ID)";
 
-			String CertLogo = jdbcTemplate.queryForObject(CertLogoSql, String.class, new Object[] { jgId,appIns });
+			String jgId = paramters[0];
+			String siteId = paramters[1];
+			String oprid = paramters[2];
+			String appIns = paramters[3];
+
+			String CertLogo = jdbcTemplate.queryForObject(CertLogoSql, String.class, new Object[] { jgId, appIns });
 			return CertLogo;
 		} catch (Exception e) {
 			e.printStackTrace();
 			return "";
 		}
 	}
-	
+
 	// 清华MBA录取通知书-个人照片url;
 	public String getCertImg(String[] paramters) {
 		try {
 			GetSpringBeanUtil getSpringBeanUtil = new GetSpringBeanUtil();
 			JdbcTemplate jdbcTemplate = (JdbcTemplate) getSpringBeanUtil.getSpringBeanByID("jdbcTemplate");
 
-			String CertImgSql="SELECT CONCAT(B.TZ_ATT_A_URL,A.TZ_ATTACHSYSFILENA) FROM PS_TZ_OPR_PHT_GL_T A,PS_TZ_OPR_PHOTO_T B WHERE A.TZ_ATTACHSYSFILENA=B.TZ_ATTACHSYSFILENA AND A.OPRID=?";
+			String CertImgSql = "SELECT CONCAT(B.TZ_ATT_A_URL,A.TZ_ATTACHSYSFILENA) FROM PS_TZ_OPR_PHT_GL_T A,PS_TZ_OPR_PHOTO_T B WHERE A.TZ_ATTACHSYSFILENA=B.TZ_ATTACHSYSFILENA AND A.OPRID=?";
 
-			String jgId=paramters[0];
-			String siteId=paramters[1];
-			String oprid=paramters[2];
-			String appIns=paramters[3];
-			
+			String jgId = paramters[0];
+			String siteId = paramters[1];
+			String oprid = paramters[2];
+			String appIns = paramters[3];
+
 			String CertImg = jdbcTemplate.queryForObject(CertImgSql, String.class, new Object[] { oprid });
 			return CertImg;
-		
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			return "";
 		}
 	}
-	
+
 	// 清华MBA录取通知书-姓名;
 	public String getCertNAME(String[] paramters) {
 		try {
 			GetSpringBeanUtil getSpringBeanUtil = new GetSpringBeanUtil();
 			JdbcTemplate jdbcTemplate = (JdbcTemplate) getSpringBeanUtil.getSpringBeanByID("jdbcTemplate");
 
-			String CertNameSql="SELECT B.TZ_REALNAME from PS_TZ_APP_INS_T A,PS_TZ_REG_USER_T B WHERE A.TZ_APP_INS_ID = ? AND B.OPRID=A.ROW_ADDED_OPRID";
+			String CertNameSql = "SELECT B.TZ_REALNAME from PS_TZ_APP_INS_T A,PS_TZ_REG_USER_T B WHERE A.TZ_APP_INS_ID = ? AND B.OPRID=A.ROW_ADDED_OPRID";
 
-			String jgId=paramters[0];
-			String siteId=paramters[1];
-			String oprid=paramters[2];
-			String appIns=paramters[3];
-			
+			String jgId = paramters[0];
+			String siteId = paramters[1];
+			String oprid = paramters[2];
+			String appIns = paramters[3];
+
 			String CertName = jdbcTemplate.queryForObject(CertNameSql, String.class, new Object[] { appIns });
 			return CertName;
-		
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			return "";
 		}
 	}
-	
+
 	// 清华MBA录取通知书-入学年份;
 	public String getCertYEAR(String[] paramters) {
 		try {
 			GetSpringBeanUtil getSpringBeanUtil = new GetSpringBeanUtil();
 			JdbcTemplate jdbcTemplate = (JdbcTemplate) getSpringBeanUtil.getSpringBeanByID("jdbcTemplate");
 
-			String CertYearSql="SELECT YEAR(A.TZ_START_DT) from PS_TZ_CLASS_INF_T A,PS_TZ_FORM_WRK_T B WHERE B.TZ_APP_INS_ID=? AND A.TZ_CLASS_ID=B.TZ_CLASS_ID";
+			String CertYearSql = "SELECT YEAR(A.TZ_START_DT) from PS_TZ_CLASS_INF_T A,PS_TZ_FORM_WRK_T B WHERE B.TZ_APP_INS_ID=? AND A.TZ_CLASS_ID=B.TZ_CLASS_ID";
 
-			String jgId=paramters[0];
-			String siteId=paramters[1];
-			String oprid=paramters[2];
-			String appIns=paramters[3];
-			
+			String jgId = paramters[0];
+			String siteId = paramters[1];
+			String oprid = paramters[2];
+			String appIns = paramters[3];
+
 			String CertYear = jdbcTemplate.queryForObject(CertYearSql, String.class, new Object[] { appIns });
 			return CertYear;
-		
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			return "";
 		}
 	}
-	
+
 	// 项目名称;
 	public String getPrgName(String[] paramters) {
 		try {
 			GetSpringBeanUtil getSpringBeanUtil = new GetSpringBeanUtil();
 			JdbcTemplate jdbcTemplate = (JdbcTemplate) getSpringBeanUtil.getSpringBeanByID("jdbcTemplate");
 
-			String PrgNameSql="SELECT B.TZ_PRJ_NAME FROM PS_TZ_APP_INS_T A,PS_TZ_PRJ_INF_T B WHERE A.TZ_APP_INS_ID=? AND A.TZ_APP_TPL_ID=B.TZ_APP_MODAL_ID";
+			String PrgNameSql = "SELECT B.TZ_PRJ_NAME FROM PS_TZ_APP_INS_T A,PS_TZ_PRJ_INF_T B WHERE A.TZ_APP_INS_ID=? AND A.TZ_APP_TPL_ID=B.TZ_APP_MODAL_ID";
 
-			String jgId=paramters[0];
-			String siteId=paramters[1];
-			String oprid=paramters[2];
-			String appIns=paramters[3];
-			
+			String jgId = paramters[0];
+			String siteId = paramters[1];
+			String oprid = paramters[2];
+			String appIns = paramters[3];
+
 			String PrgName = jdbcTemplate.queryForObject(PrgNameSql, String.class, new Object[] { appIns });
 			return PrgName;
-		
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+
+	// 企业Id / 公众号appId;
+	public String getWxCorpid(String[] paramters) {
+		try {
+			GetSpringBeanUtil getSpringBeanUtil = new GetSpringBeanUtil();
+			JdbcTemplate jdbcTemplate = (JdbcTemplate) getSpringBeanUtil.getSpringBeanByID("jdbcTemplate");
+
+			String WxCorpidSql = "SELECT TZ_HARDCODE_VAL FROM PS_TZ_HARDCD_PNT WHERE TZ_HARDCODE_PNT=?";
+			String WxCorpid = jdbcTemplate.queryForObject(WxCorpidSql, String.class, new Object[] { "TZ_WX_CORPID" });
+			return WxCorpid;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+
+	// 参与签名的字段noncestr（随机字符串）;
+	public String getWxNonce_str(String[] paramters) {
+			try {
+				GetSpringBeanUtil getSpringBeanUtil = new GetSpringBeanUtil();
+				JdbcTemplate jdbcTemplate = (JdbcTemplate) getSpringBeanUtil.getSpringBeanByID("jdbcTemplate");
+
+				String jgId=paramters[0];
+				String siteId=paramters[1];
+				String oprid=paramters[2];
+				String appIns=paramters[3];
+				
+				/*微信签名信息*/			
+				String sqlHardCode="SELECT TZ_HARDCODE_VAL FROM PS_TZ_HARDCD_PNT WHERE TZ_HARDCODE_PNT=?";
+				String WxCorpid = jdbcTemplate.queryForObject(sqlHardCode, String.class, new Object[] { "TZ_WX_CORPID" });
+				String WxSecret = jdbcTemplate.queryForObject(sqlHardCode, String.class, new Object[] { "TZ_WX_SECRET" });
+				String WxType = jdbcTemplate.queryForObject(sqlHardCode, String.class, new Object[] { "TZ_WX_TYPE" });
+				
+				HttpServletRequest httpServletRequest= (HttpServletRequest) getSpringBeanUtil.getSpringBeanByID("httpServletRequest");				
+				String url = httpServletRequest.getScheme() + "://" + httpServletRequest.getServerName() + ":"
+						+ String.valueOf(httpServletRequest.getServerPort()) + httpServletRequest.getContextPath() + "/admission/" +jgId+"/" + siteId + "/" + oprid
+						+ "/" + appIns;
+			
+				TzWeChartJSSDKSign tzWeChartJSSDKSign =  (TzWeChartJSSDKSign) getSpringBeanUtil.getSpringBeanByID("tzWeChartJSSDKSign");
+				Map<String, String> ret=tzWeChartJSSDKSign.sign(WxCorpid, WxSecret, WxType, url);
+				String WxNonce_str=ret.get("nonceStr");
+				
+				return WxNonce_str;
+			} catch (Exception e) {
+				e.printStackTrace();
+				return "";
+			}
+		}
+
+	// 参与签名的字段noncestr（时间戳）;
+	public String getWxTimestamp(String[] paramters) {
+		try {
+			GetSpringBeanUtil getSpringBeanUtil = new GetSpringBeanUtil();
+			JdbcTemplate jdbcTemplate = (JdbcTemplate) getSpringBeanUtil.getSpringBeanByID("jdbcTemplate");
+
+			String jgId = paramters[0];
+			String siteId = paramters[1];
+			String oprid = paramters[2];
+			String appIns = paramters[3];
+
+			/* 微信签名信息 */
+			String sqlHardCode = "SELECT TZ_HARDCODE_VAL FROM PS_TZ_HARDCD_PNT WHERE TZ_HARDCODE_PNT=?";
+			String WxCorpid = jdbcTemplate.queryForObject(sqlHardCode, String.class, new Object[] { "TZ_WX_CORPID" });
+			String WxSecret = jdbcTemplate.queryForObject(sqlHardCode, String.class, new Object[] { "TZ_WX_SECRET" });
+			String WxType = jdbcTemplate.queryForObject(sqlHardCode, String.class, new Object[] { "TZ_WX_TYPE" });
+
+			HttpServletRequest httpServletRequest = (HttpServletRequest) getSpringBeanUtil
+					.getSpringBeanByID("httpServletRequest");
+			String url = httpServletRequest.getScheme() + "://" + httpServletRequest.getServerName() + ":"
+					+ String.valueOf(httpServletRequest.getServerPort()) + httpServletRequest.getContextPath()
+					+ "/admission/" + jgId + "/" + siteId + "/" + oprid + "/" + appIns;
+
+			TzWeChartJSSDKSign tzWeChartJSSDKSign = (TzWeChartJSSDKSign) getSpringBeanUtil
+					.getSpringBeanByID("tzWeChartJSSDKSign");
+			Map<String, String> ret = tzWeChartJSSDKSign.sign(WxCorpid, WxSecret, WxType, url);
+			String WxTimestamp = ret.get("timestamp");
+
+			return WxTimestamp;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+
+	// 参与签名的字段signature（签名）;
+	public String getWxSignature(String[] paramters) {
+		try {
+			GetSpringBeanUtil getSpringBeanUtil = new GetSpringBeanUtil();
+			JdbcTemplate jdbcTemplate = (JdbcTemplate) getSpringBeanUtil.getSpringBeanByID("jdbcTemplate");
+
+			String jgId = paramters[0];
+			String siteId = paramters[1];
+			String oprid = paramters[2];
+			String appIns = paramters[3];
+
+			/* 微信签名信息 */
+			String sqlHardCode = "SELECT TZ_HARDCODE_VAL FROM PS_TZ_HARDCD_PNT WHERE TZ_HARDCODE_PNT=?";
+			String WxCorpid = jdbcTemplate.queryForObject(sqlHardCode, String.class, new Object[] { "TZ_WX_CORPID" });
+			String WxSecret = jdbcTemplate.queryForObject(sqlHardCode, String.class, new Object[] { "TZ_WX_SECRET" });
+			String WxType = jdbcTemplate.queryForObject(sqlHardCode, String.class, new Object[] { "TZ_WX_TYPE" });
+
+			HttpServletRequest httpServletRequest = (HttpServletRequest) getSpringBeanUtil
+					.getSpringBeanByID("httpServletRequest");
+			String url = httpServletRequest.getScheme() + "://" + httpServletRequest.getServerName() + ":"
+					+ String.valueOf(httpServletRequest.getServerPort()) + httpServletRequest.getContextPath()
+					+ "/admission/" + jgId + "/" + siteId + "/" + oprid + "/" + appIns;
+
+			TzWeChartJSSDKSign tzWeChartJSSDKSign = (TzWeChartJSSDKSign) getSpringBeanUtil
+					.getSpringBeanByID("tzWeChartJSSDKSign");
+			Map<String, String> ret = tzWeChartJSSDKSign.sign(WxCorpid, WxSecret, WxType, url);
+			String WxSignature = ret.get("signature");
+
+			return WxSignature;
 		} catch (Exception e) {
 			e.printStackTrace();
 			return "";
