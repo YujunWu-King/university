1aa9c25c4df6d085ac1e291d02c0368a5fe98430
顾贤达 <guxd@tranzvision.com.cn>
2017-04-05 14:20:13 +0800

    自动化打分内容

---
+++ src/main/java/com/tranzvision/gd/TZUnifiedBaseBundle/model/PsTzCjxTblKey.java
@@ -0,0 +1,23 @@
+package com.tranzvision.gd.TZUnifiedBaseBundle.model;
+
+public class PsTzCjxTblKey {
+    private Long tzScoreInsId;
+
+    private String tzScoreItemId;
+
+    public Long getTzScoreInsId() {
+        return tzScoreInsId;
+    }
+
+    public void setTzScoreInsId(Long tzScoreInsId) {
+        this.tzScoreInsId = tzScoreInsId;
+    }
+
+    public String getTzScoreItemId() {
+        return tzScoreItemId;
+    }
+
+    public void setTzScoreItemId(String tzScoreItemId) {
+        this.tzScoreItemId = tzScoreItemId == null ? null : tzScoreItemId.trim();
+    }
+}
