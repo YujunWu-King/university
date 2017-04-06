353f40edbb65965607b6217aca7d7b22ba3e5f2c
叶少威 <shaowei.ye@tranzvision.com.cn>
2017-04-06 17:17:32 +0800

    材料和面试评审评委。

---
+++ src/main/java/com/tranzvision/gd/TZEvaluationSystemBundle/service/impl/MaterialEvaluationCls.java
@@ -0,0 +1,28 @@
+package com.tranzvision.gd.TZEvaluationSystemBundle.service.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tranzvision.gd.TZAuthBundle.service.impl.TzLoginServiceImpl;
+import com.tranzvision.gd.TZBaseBundle.service.impl.FrameworkImpl;
+import com.tranzvision.gd.util.base.JacksonUtil;
+import com.tranzvision.gd.util.sql.SqlQuery;
+import com.tranzvision.gd.util.sql.TZGDObject;
+
+/**
+ * 材料面试评审相关类
+ * 
+ * @author ShaweYet
+ * @since 2017/03/06
+ */
+public class MaterialEvaluationCls{
+	
+	
+}
