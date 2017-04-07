daf39ce8632e6e517b32e517c57d2dd04f154cee
琚峰 <jufeng@tranzvision.com.cn>
2017-04-07 13:01:48 +0800

    站内信模板推荐人参数修改
    Signed-off-by: 琚峰 <jufeng@tranzvision.com.cn>

--- src/main/java/com/tranzvision/gd/TZZnxTemplateBundle/service/impl/ZnxGetParamter.java
+++ src/main/java/com/tranzvision/gd/TZZnxTemplateBundle/service/impl/ZnxGetParamter.java
@@ -119,7 +119,7 @@ public class ZnxGetParamter {
 			String bmbId = jdbcTemplate.queryForObject(sql, String.class, new Object[] { audId, audCyId });
 			if (bmbId != null && !"".equals(bmbId)){
 				//String referrerSql = "SELECT TZ_REFERRER_NAME FROM PS_TZ_KS_TJX_TBL WHERE TZ_REF_LETTER_ID =?";
-				String referrerSql = "SELECT TZ_REALNAME FROM PS_TZ_AQ_YHXX_TBL WHERE OPRID = (SELECT OPRID FROM PS_TZ_KS_TJX_TBL WHERE TZ_APP_INS_ID=?)";
+				String referrerSql = "SELECT TZ_REFERRER_NAME FROM PS_TZ_KS_TJX_TBL WHERE TZ_APP_INS_ID = ?";
 				String referrerName = jdbcTemplate.queryForObject(referrerSql, String.class, new Object[] { bmbId });
 				return referrerName;
 			}else{
