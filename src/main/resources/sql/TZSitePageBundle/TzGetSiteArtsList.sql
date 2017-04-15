ba679f2208be1840b6c99c079663b2fccdc5f3a6
琚峰 <jufeng@tranzvision.com.cn>
2017-04-15 13:56:15 +0800

    手机版站内信样式修改
    Signed-off-by: 琚峰 <jufeng@tranzvision.com.cn>

--- src/main/resources/sql/TZSitePageBundle/TzGetSiteArtsList.sql
+++ src/main/resources/sql/TZSitePageBundle/TzGetSiteArtsList.sql
@@ -14,10 +14,6 @@ where
     and PT2.TZ_ART_PUB_STATE = 'Y' 
     and PT1.TZ_SITEI_ID = ? 
     and PT1.TZ_COLU_ID = ?
-    and (PT3.TZ_PROJECT_LIMIT<>'B' or 
-		exists (select 1 from PS_TZ_ART_AUDIENCE_T AUD 
-		inner join PS_TZ_AUD_LIST_T LST on(AUD.TZ_AUD_ID = LST.TZ_AUD_ID and LST.OPRID=?) 
-		where AUD.TZ_ART_ID=PT3.TZ_ART_ID))
 order by 
 	PT2.TZ_MAX_ZD_SEQ desc, 
 	PT2.TZ_ART_SEQ desc,
