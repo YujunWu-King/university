8776e15bfac62aa3713b160627a1004d24e7143b
zhangbinbin <zhangbb@tranzvision.com.cn>
2017-04-18 10:51:29 +0800

    报名表导出修改

--- src/main/java/com/tranzvision/gd/TZApplicationVerifiedBundle/service/impl/TzGdBmgExcelUtility.java
+++ src/main/java/com/tranzvision/gd/TZApplicationVerifiedBundle/service/impl/TzGdBmgExcelUtility.java
@@ -83,28 +83,116 @@ public class TzGdBmgExcelUtility {
 		return strBmrSqYear;	
 	}
 	
+	/*获取报名人姓名*/
+	public String getBmrRealName(String strAppInsId ,String strAppTplId) throws Exception
+	{
+		Long appInsId = Long.parseLong(strAppInsId);
+		
+		String strBmrRealName = "";
+		
+		strBmrRealName = jdbcTemplate.queryForObject("SELECT TZ_REALNAME FROM PS_TZ_FORM_WRK_T A,PS_TZ_REG_USER_T B "
+				+ " WHERE A.TZ_APP_INS_ID = ? AND A.OPRID = B.OPRID LIMIT 0,1",new Object[]{appInsId},String.class);
+		
+		if(strBmrRealName==null){
+			strBmrRealName = "";
+		}
+		
+		return strBmrRealName;	
+	}
+	
+	/*获取报名人性别*/
+	public String getBmrSex(String strAppInsId ,String strAppTplId) throws Exception
+	{
+		Long appInsId = Long.parseLong(strAppInsId);
+		
+		String strBmrSex = "";
+		
+		strBmrSex = jdbcTemplate.queryForObject("SELECT CASE B.TZ_GENDER WHEN 'M' THEN '男' WHEN 'F' THEN '女' ELSE '' END AS TZ_GENDER FROM PS_TZ_FORM_WRK_T A,PS_TZ_REG_USER_T B "
+				+ " WHERE A.TZ_APP_INS_ID = ? AND A.OPRID = B.OPRID LIMIT 0,1",new Object[]{appInsId},String.class);
+		
+		if(strBmrSex==null){
+			strBmrSex = "";
+		}
+		
+		return strBmrSex;	
+	}
+	
+	/*获取报名人证件号码*/
+	public String getBmrNationalId(String strAppInsId ,String strAppTplId) throws Exception
+	{
+		Long appInsId = Long.parseLong(strAppInsId);
+		
+		String strBmrNationalId = "";
+		
+		strBmrNationalId = jdbcTemplate.queryForObject("SELECT NATIONAL_ID FROM PS_TZ_FORM_WRK_T A,PS_TZ_REG_USER_T B "
+				+ " WHERE A.TZ_APP_INS_ID = ? AND A.OPRID = B.OPRID LIMIT 0,1",new Object[]{appInsId},String.class);
+		
+		if(strBmrNationalId==null){
+			strBmrNationalId = "";
+		}
+		
+		return strBmrNationalId;	
+	}
+	
+	/*获取报名人面试申请号*/
+	public String getBmrMshId(String strAppInsId ,String strAppTplId) throws Exception
+	{
+		Long appInsId = Long.parseLong(strAppInsId);
+		
+		String strBmrMshId = "";
+		
+		strBmrMshId = jdbcTemplate.queryForObject("SELECT TZ_MSSQH FROM PS_TZ_FORM_WRK_T A,PS_TZ_REG_USER_T B "
+				+ " WHERE A.TZ_APP_INS_ID = ? AND A.OPRID = B.OPRID LIMIT 0,1",new Object[]{appInsId},String.class);
+		
+		if(strBmrMshId==null){
+			strBmrMshId = "";
+		}
+		
+		return strBmrMshId;	
+	}
+	
+	/*获取报名人出生日期*/
+	public String getBmrBirthday(String strAppInsId ,String strAppTplId) throws Exception
+	{
+		Long appInsId = Long.parseLong(strAppInsId);
+		
+		String strBmrBirthday = "";
+		
+		strBmrBirthday = jdbcTemplate.queryForObject("SELECT BIRTHDATE FROM PS_TZ_FORM_WRK_T A,PS_TZ_REG_USER_T B "
+				+ " WHERE A.TZ_APP_INS_ID = ? AND A.OPRID = B.OPRID LIMIT 0,1",new Object[]{appInsId},String.class);
+		
+		if(strBmrBirthday==null){
+			strBmrBirthday = "";
+		}
+		
+		return strBmrBirthday;	
+	}
+	
 	/*获取报名人年龄*/
 	public String getBmrAge(String strAppInsId ,String strAppTplId) throws Exception
 	{
 		Long appInsId = Long.parseLong(strAppInsId);
 		
-		String strBmrBirthdayBhxxx = "";
+		//String strBmrBirthdayBhxxx = "";
 		
 		String strBmrBirthday = "";
 		
 		Date dtBmrBirthday;
 		
-		strBmrBirthdayBhxxx = jdbcTemplate.queryForObject("SELECT TZ_HARDCODE_VAL FROM PS_TZ_HARDCD_PNT WHERE TZ_HARDCODE_PNT = ? LIMIT 0,1",
-						new Object[]{"TZ_BMR_BIRTHDAY_XXXBH"},String.class);
 		String strBmrAge = "";
 		
+		/*strBmrBirthdayBhxxx = jdbcTemplate.queryForObject("SELECT TZ_HARDCODE_VAL FROM PS_TZ_HARDCD_PNT WHERE TZ_HARDCODE_PNT = ? LIMIT 0,1",
+						new Object[]{"TZ_BMR_BIRTHDAY_XXXBH"},String.class);
+						
 		strBmrBirthday = jdbcTemplate.queryForObject("SELECT TZ_APP_S_TEXT FROM PS_TZ_APP_CC_T A WHERE TZ_APP_INS_ID= ? AND TZ_XXX_BH= ? LIMIT 0,1",
 				new Object[]{appInsId,strBmrBirthdayBhxxx},String.class);
-		
+		*/
+
+		strBmrBirthday = jdbcTemplate.queryForObject("SELECT BIRTHDATE FROM PS_TZ_FORM_WRK_T A,PS_TZ_REG_USER_T B "
+				+ " WHERE A.TZ_APP_INS_ID = ? AND A.OPRID = B.OPRID LIMIT 0,1",new Object[]{appInsId},String.class);
 		
 		String strBmrRxdt = "";
 		Date dtBmrRxdt;
-		
 		
 		strBmrRxdt = jdbcTemplate.queryForObject("SELECT TZ_RX_DT FROM PS_TZ_FORM_WRK_T A,PS_TZ_CLASS_INF_T B "
 				+ " WHERE A.TZ_APP_INS_ID = ? AND A.TZ_CLASS_ID = B.TZ_CLASS_ID LIMIT 0,1",new Object[]{appInsId},String.class);
