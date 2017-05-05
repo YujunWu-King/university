bcd917f3bbd99126e8b6524ac0a30123659e53a4
宋子成 <songzc@tranzvision.com.cn>
2017-05-05 18:42:44 +0800

    1

--- src/main/webapp/statics/js/tranzvision/extjs/app/view/enrollmentManagement/materialsReview/materialsReviewSchedule.js
+++ src/main/webapp/statics/js/tranzvision/extjs/app/view/enrollmentManagement/materialsReview/materialsReviewSchedule.js
@@ -123,6 +123,7 @@ Ext.define('KitchenSink.view.enrollmentManagement.materialsReview.materialsRevie
             title: '图表区',
             collapsible: true,
             collapsed: true,
+            scrollable: true,
             plugins: {
                 ptype: 'maximize'
             },
@@ -1189,10 +1190,14 @@ Ext.define('KitchenSink.view.enrollmentManagement.materialsReview.materialsRevie
 	                                		lineHeigth = (pwNum + 1) * 20 + 400;
 	                                		// 保持柱状图和线状图宽度一致
 	                                		if (columnWidth > lineWidth) {
-	                                			lineWidth = columnWidth;
+	                                			//lineWidth = columnWidth;
 	                                		} else {
-	                                			columnWidth = lineWidth;
+	                                			//columnWidth = lineWidth;
 	                                		}
+	                                		// 对曲线图增加点宽度;
+	                                		lineWidth = lineWidth + (pwNum + 1) * 66; 
+	                                		
+	                                		
 	                                		// ------------------------------------柱状图开始---------------------------
 	                                		// 1、生产柱状图数据
 	                                		for (var i = 0; i < pwArr.length; i++) {
