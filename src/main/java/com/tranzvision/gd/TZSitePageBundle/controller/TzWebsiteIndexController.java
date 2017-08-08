002950d4271f83d0f8b4aaa48cf402927575e555
叶少威 <shaowei.ye@tranzvision.com.cn>
2017-08-08 15:20:29 +0800

    PC版首页自动检查手机版调整：解决Mac Safari访问报考服务系统首页跳转到移动端页面

--- src/main/java/com/tranzvision/gd/TZSitePageBundle/controller/TzWebsiteIndexController.java
+++ src/main/java/com/tranzvision/gd/TZSitePageBundle/controller/TzWebsiteIndexController.java
@@ -17,6 +17,7 @@ import com.tranzvision.gd.TZSitePageBundle.service.impl.TzWebsiteServiceImpl;
 import com.tranzvision.gd.TZMobileWebsiteIndexBundle.service.impl.MobileWebsiteIndexServiceImpl;
 import com.tranzvision.gd.util.base.TzSystemException;
 import com.tranzvision.gd.util.sql.TZGDObject;
+import com.tranzvision.gd.util.httpclient.CommonUtils;
 
 /**
  * 网站首页展示
@@ -48,7 +49,7 @@ public class TzWebsiteIndexController {
 		orgid = orgid.toLowerCase();
 		String strRet = "";
 		
-		if(isMoblie(request)){
+		if(CommonUtils.isMobile(request)){
 			String strParams = "{\"siteId\":\""+siteid+"\"}";
 			strRet = mobileWebsiteIndexServiceImpl.tzGetHtmlContent(strParams);
 		}else{
@@ -83,31 +84,4 @@ public class TzWebsiteIndexController {
 		return strRet;
 	}
 
-	public boolean isMoblie(HttpServletRequest request) {
-		boolean isMoblie = false;
-		String[] mobileAgents = { "iphone", "android", "phone", "mobile", "wap", "netfront", "java", "opera mobi",
-				"opera mini", "ucweb", "windows ce", "symbian", "series", "webos", "sony", "blackberry", "dopod",
-				"nokia", "samsung", "palmsource", "xda", "pieplus", "meizu", "midp", "cldc", "motorola", "foma",
-				"docomo", "up.browser", "up.link", "blazer", "helio", "hosin", "huawei", "novarra", "coolpad", "webos",
-				"techfaith", "palmsource", "alcatel", "amoi", "ktouch", "nexian", "ericsson", "philips", "sagem",
-				"wellcom", "bunjalloo", "maui", "smartphone", "iemobile", "spice", "bird", "zte-", "longcos",
-				"pantech", "gionee", "portalmmm", "jig browser", "hiptop", "benq", "haier", "^lct", "320x320",
-				"240x320", "176x220", "w3c ", "acs-", "alav", "alca", "amoi", "audi", "avan", "benq", "bird", "blac",
-				"blaz", "brew", "cell", "cldc", "cmd-", "dang", "doco", "eric", "hipt", "inno", "ipaq", "java", "jigs",
-				"kddi", "keji", "leno", "lg-c", "lg-d", "lg-g", "lge-", "maui", "maxo", "midp", "mits", "mmef", "mobi",
-				"mot-", "moto", "mwbp", "nec-", "newt", "noki", "oper", "palm", "pana", "pant", "phil", "play", "port",
-				"prox", "qwap", "sage", "sams", "sany", "sch-", "sec-", "send", "seri", "sgh-", "shar", "sie-", "siem",
-				"smal", "smar", "sony", "sph-", "symb", "t-mo", "teli", "tim-", "tosh", "tsm-", "upg1", "upsi", "vk-v",
-				"voda", "wap-", "wapa", "wapi", "wapp", "wapr", "webc", "winw", "winw", "xda", "xda-",
-				"Googlebot-Mobile" };
-		if (request.getHeader("User-Agent") != null) {
-			for (String mobileAgent : mobileAgents) {
-				if (request.getHeader("User-Agent").toLowerCase().indexOf(mobileAgent) >= 0) {
-					isMoblie = true;
-					break;
-				}
-			}
-		}
-		return isMoblie;
-	}
 }
