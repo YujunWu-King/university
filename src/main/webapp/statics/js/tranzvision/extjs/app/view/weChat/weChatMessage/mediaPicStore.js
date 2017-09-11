61ab87209592655c940ae03fda555b0540b6b541
zhanglang <zhanglang@tranzvision.com.cn>
2017-09-11 13:43:10 +0800

    微信图片选择

--- src/main/webapp/statics/js/tranzvision/extjs/app/view/weChat/weChatMessage/mediaPicStore.js
+++ src/main/webapp/statics/js/tranzvision/extjs/app/view/weChat/weChatMessage/mediaPicStore.js
@@ -2,10 +2,19 @@ Ext.define('KitchenSink.view.weChat.weChatMessage.mediaPicStore', {
     extend: 'Ext.data.Store',
     alias: 'store.mediaPicStore',
     model: 'KitchenSink.view.weChat.weChatMessage.mediaPicModel',
-    autoLoad:false,
+    autoLoad:true,
     comID: 'TZ_GD_WXMSG_COM',
 	pageID: 'TZ_GD_SCGL_STD',
 	tzStoreParams: '',
     pageSize: 10, 
-    proxy: Ext.tzListProxy()
+    proxy: Ext.tzListProxy(),
+    
+    constructor:function(config){
+        var materialType = config.materialType;
+        var wxAppId = config.wxAppId;
+        
+        this.tzStoreParams = '{"wxAppId":"'+wxAppId+'","mediaType":"'+materialType+'"}';
+        
+        this.callParent();
+    },
 });
