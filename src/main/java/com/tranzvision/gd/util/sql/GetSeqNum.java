2a1ce4324eddbace81f716b39202df42a126080e
Jollow <ligang@tranzvision.com.cn>
2017-10-23 12:56:46 +0800

    2017年10月23日，为解决Java版高端产品MySQL数据库死锁问题对程序/com/tranzvision/gd/util/sql/GetSeqNum.java进行优化，并增加一个辅助Java类程序/com/tranzvision/gd/util/sql/QuickSequenceObject.java。

--- src/main/java/com/tranzvision/gd/util/sql/GetSeqNum.java
+++ src/main/java/com/tranzvision/gd/util/sql/GetSeqNum.java
@@ -3,6 +3,8 @@ package com.tranzvision.gd.util.sql;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Semaphore;
@@ -16,12 +18,28 @@ public class GetSeqNum
 	@Autowired
 	private TZGDObject tzGDObject;
 	
-	//记录当前服务连续获取下一个序列号次数的变量
-	private static HashMap<String,Integer> continuousTimesMap = new HashMap<String,Integer>();
-	//记录当前服务上次获取的下一个序列号的变量
-	private static HashMap<String,Integer> previousIndexMap = new HashMap<String,Integer>();
+	//记录当前服务获取序列状态对象的映射集合变量
+	private static HashMap<String,QuickSequenceObject> currentQueneLengthMap = new HashMap<String,QuickSequenceObject>();
 	
+	//public int getSeqNum(String tableName, String colName) throws Exception
 	public int getSeqNum(String tableName, String colName)
+	{
+		int index = 0;
+		
+		
+		//获取序列号
+		index = this.pGetSeqNum1(tableName,colName);
+		if(index <= 0)
+		{
+			//throw new Exception("failed to acquire the next sequence number for [" + tableName + "." + colName + "].");
+		}
+		
+		
+		return index;
+	}
+	
+	
+	private int pGetSeqNum1(String tableName, String colName)
 	{
 		int index = 0;
 		
@@ -40,61 +58,51 @@ public class GetSeqNum
 		try
 		{
 			//通过获取的信号灯将获取下一个序列值的并行访问串行化执行
-			tmpSemaphore.acquire();
+			tmpSemaphore.acquireUninterruptibly();
 			
 			
-			if(continuousTimesMap.containsKey(tmpSemaphoreName) == false)
+			//生成记录上次序列状态的对象
+			if(currentQueneLengthMap.containsKey(tmpSemaphoreName) == false)
 			{
-				continuousTimesMap.put(tmpSemaphoreName, new Integer(0));
-			}
-			if(previousIndexMap.containsKey(tmpSemaphoreName) == false)
-			{
-				previousIndexMap.put(tmpSemaphoreName, new Integer(0));
+				currentQueneLengthMap.put(tmpSemaphoreName,new QuickSequenceObject());
 			}
 			
+			QuickSequenceObject tmpQuickSequenceObject = currentQueneLengthMap.get(tmpSemaphoreName);
 			
 			try
 			{
-				int continuoustimes = continuousTimesMap.get(tmpSemaphoreName);
-				int previousindex = previousIndexMap.get(tmpSemaphoreName);
+				//如果上次记录了当前序列的排队参数，则直接通过上次记录的排队参数来获取下一个序列号，以减少对数据库中序列对象的争用，减少数据库发送死锁的概率
+				index = tmpQuickSequenceObject.getNextSequenceNumber();
 				
-				//如果尝试20次还没有获得下一个序列号，则直接返回0，返回0表示获取下一个序列号失败
-				int counter = 0;
-				while(index <= 0)
-				{
-					//获取下一个序列号值，并计数
-					index = this.pGetSeqNum(tableName, colName);
-					counter ++;
-					
-					//如果成功获得下一个序列号（index的值大于0），则记录该值，并将continuoustimes的值增加1
-					if(index >= 1)
+				if(index <= 0)
+				{//否则通过数据库中的序列对象来获取指定序列的下一个值
+					//如果尝试1000次还没有获得下一个序列号，则直接返回0，返回0表示获取下一个序列号失败
+					int counter = 0;
+					while(index <= 0)
 					{
-						if(index - previousindex == 1)
-						{
-							continuoustimes ++;
-						}
-						else
-						{
-							continuoustimes = 0;
-						}
-						previousindex = index;
+						//获取下一个序列号值，并计数
+						index = this.pGetSeqNum2(tableName, colName, tmpQuickSequenceObject, tmpSemaphore.getQueueLength());
+						counter ++;
 						
-						//如果针对同一个序列，当前服务已连续获得了10个连续的序列值，则让它睡眠20毫秒，以便其他的服务有机会获得针对同一序列的序列值的机会
-						if(continuoustimes >= 10)
+						//每成功抢到一次就睡眠10毫秒
+						if(index >= 1)
 						{
-							continuoustimes = 0;
-							System.out.println("the current thread[" + Thread.currentThread().getId() + "] will sleep 20 milliseconds.");
-							Thread.sleep(20);
+							try
+							{
+								Thread.sleep(10);
+							}
+							catch(Exception e)
+							{
+								//do nothing
+							}
 						}
 						
-						continuousTimesMap.put(tmpSemaphoreName, continuoustimes);
-						previousIndexMap.put(tmpSemaphoreName, previousindex);
-					}
-					
-					//如果超过20次都没有成功获得下一个序列号，则宣告失败并返回0
-					if(counter >= 20)
-					{
-						break;
+						//如果超过100次都没有成功获得下一个序列号，则宣告失败并返回0
+						if(counter >= 1000)
+						{
+							System.err.println((new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")).format(new Date()) + "ERROR, failed to get the next sequence value for " + tableName + "." + colName + " in the 1000 times cycle.");
+							break;
+						}
 					}
 				}
 			}
@@ -117,7 +125,7 @@ public class GetSeqNum
 	}
 
 
-	private int pGetSeqNum(String tableName, String colName)
+	private int pGetSeqNum2(String tableName, String colName, QuickSequenceObject quickSequenceObject, int queneLength)
 	{
 		int index = 0;
 
@@ -151,10 +159,11 @@ public class GetSeqNum
 			tmpIndex = jdbcTemplate.queryForObject(sql, new Object[] { tableName, colName }, "int");
 			
 			
-			//更新当前序列的值
+			//更新当前序列的值，queneLength记录了当前正在排队请求获取当前指定序列序列值的队列长度，如果队列长度大于等于1，则直接将数据库中对应的序列对象的当前序列值更新为TZ_SEQNUM+1+queneLength
+			//这样后续队列中的queneLength个请求直接通过后续记录的序列值和队列长度计算获得，以减少对数据库序列对象的争用，减少数据库死锁发生的概率
 			int updateFlag = 0;
-			sql = "UPDATE PS_TZ_SEQNUM_T SET TZ_SEQNUM=TZ_SEQNUM+1 WHERE TZ_TABLE_NAME = ? AND TZ_COL_NAME = ? AND TZ_SEQNUM <= ?";
-			updateFlag = jdbcTemplate.update(sql, new Object[] { tableName, colName, tmpIndex});
+			sql = "UPDATE PS_TZ_SEQNUM_T SET TZ_SEQNUM=TZ_SEQNUM+1+? WHERE TZ_TABLE_NAME = ? AND TZ_COL_NAME = ? AND TZ_SEQNUM <= ?";
+			updateFlag = jdbcTemplate.update(sql, new Object[] { queneLength, tableName, colName, tmpIndex});
 			
 			
 			//此处利用jdbcTemplate.update方法返回更新记录行数来判断是否更新成功
@@ -162,10 +171,17 @@ public class GetSeqNum
 			{
 				//如果更新成功，则返回下一个序列值
 				index = tmpIndex + 1;
+				
+				//记录当前序列的排队参数，如果排队队列长度大于等于1，则记录当前的已获取的序列值和序列长度，以便后续请求中可以使用这两个排队参数直接获取下一个序列值
+				if(queneLength >= 1)
+				{
+					quickSequenceObject.set(index, queneLength);
+				}
 			}
 		}
 		catch (Exception e)
 		{
+			System.err.println("failed to get the next sequence number for the sequence[" + tableName + "." + colName + "] because of the exception: " + e.toString());
 			//如果发生异常直接返回0，表示获取下一个序列号失败
 			return 0;
 		}
