28583ecfac87adf250df46fa164f281b86b1bc55
luyan <luyan@tranzvision.com.cn>
2017-12-12 11:38:05 +0800

    统一打印模板列表状态直接在视图取值

--- src/main/webapp/statics/js/tranzvision/extjs/app/view/uniPrint/uniPrintTplModel.js
+++ src/main/webapp/statics/js/tranzvision/extjs/app/view/uniPrint/uniPrintTplModel.js
@@ -5,6 +5,7 @@ Ext.define('KitchenSink.view.uniPrint.uniPrintTplModel', {
         {name: 'TZ_DYMB_ID'},
         {name: 'TZ_DYMB_NAME'},
         {name: 'TZ_DYMB_ZT'},
+        {name: 'TZ_DYMB_ZT_DESC'},
         {name: 'TZ_TPL_NAME'},
         {name: 'TZ_DYMB_MENO'},
         {name: 'TZ_DYMB_PDF_URL'}
