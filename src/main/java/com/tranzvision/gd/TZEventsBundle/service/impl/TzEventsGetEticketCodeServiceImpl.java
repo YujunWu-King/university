5c097b042cb34531671dab4f5904abd5c1b21826
zhanglang <zhanglang@tranzvision.com.cn>
2018-03-30 16:53:35 +0800

    活动签到二维码修改

--- src/main/java/com/tranzvision/gd/TZEventsBundle/service/impl/TzEventsGetEticketCodeServiceImpl.java
+++ src/main/java/com/tranzvision/gd/TZEventsBundle/service/impl/TzEventsGetEticketCodeServiceImpl.java
@@ -4,12 +4,13 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import com.tranzvision.gd.util.base.GetSpringBeanUtil;
 import com.tranzvision.gd.util.qrcode.QRCode;
-import com.tranzvision.gd.util.sql.SqlQuery;
+import com.tranzvision.gd.util.sql.TZGDObject;
 /***
  * 生产活动签到码二维码
  * @author tzhjl
@@ -19,12 +20,6 @@ import com.tranzvision.gd.util.sql.SqlQuery;
 public  class TzEventsGetEticketCodeServiceImpl   {
 	private String charset = "UTF-8";
 	
-	private static String TZ_ACTCODE_REPATCH="";
-	
-	private static String TZ_ACTCODE_TUREPATCH="";
-	
-	private static String TZ_COMTENTURL="";
-	
 	/***
 	 * @param actId 活动ID
 	 * @param actQdid 活动签到码ID
@@ -33,15 +28,12 @@ public  class TzEventsGetEticketCodeServiceImpl   {
 	public String getHdTicketCode( String actId ,String actQdid) {
 		String ticketCode="";
 		try {
-			
 			//二维码图片名称
 			String fileName="TZEVENTS_"+actId+'_'+actQdid+".png";
 			//二维码图片 签到码内容
 			String encoderContent = "{\"QDM\":"+actQdid+"}";
 			
-			ticketCode=this.encodeQRCode("SEM", encoderContent, fileName,charset, 200, 200);
-			
-			System.out.println("ticketCode==="+ticketCode);
+			ticketCode=this.encodeQRCode(encoderContent, fileName,charset, 150, 150);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -55,8 +47,6 @@ public  class TzEventsGetEticketCodeServiceImpl   {
 	
 	/**
 	 * 完整参数
-	 * 
-	 * @param orgid
 	 * @param qrCodeInfo
 	 * @param fileName
 	 * @param charSet
@@ -64,46 +54,39 @@ public  class TzEventsGetEticketCodeServiceImpl   {
 	 * @param qrCodeHeight
 	 * @return String 二维码图片访问路径
 	 */
-	public String encodeQRCode(String orgid, String qrCodeInfo, String fileName, String charSet, int qrCodeWidth,
-			int qrCodeHeight) {
+	public String encodeQRCode(String qrCodeInfo, String fileName, String charSet, int qrCodeWidth,int qrCodeHeight) {
+		String strReturn = "";
+		
 		GetSpringBeanUtil getSpringBeanUtil = new GetSpringBeanUtil();
-		SqlQuery sqlQuery = (SqlQuery) getSpringBeanUtil.getAutowiredSpringBean("SqlQuery");
+		TZGDObject tzGDObject = (TZGDObject) getSpringBeanUtil.getSpringBeanByID("TZGDObject");
 
-		String strReturn="";
-		if (null == orgid || "".equals(orgid)) {
-			return "";
-		}				
-		String hardcodeSql = "select TZ_HARDCODE_VAL from PS_TZ_HARDCD_PNT where TZ_HARDCODE_PNT=?";
-		//相对路径
-		TZ_ACTCODE_REPATCH = sqlQuery.queryForObject(hardcodeSql, new Object[] { "TZ_ACTCODE_REPATCH" },
-				"String");
-		//服务器路径
-		TZ_ACTCODE_TUREPATCH = sqlQuery.queryForObject(hardcodeSql, new Object[] { "TZ_ACTCODE_TUREPATCH" },
-				"String");
-		//访问路径
-		TZ_COMTENTURL = sqlQuery.queryForObject(hardcodeSql, new Object[] { "TZ_COMTENTURL" },
-				"String");
-		//String qrcodeFilePath = "/statics/images/website/qrcode/" + orgid.toLowerCase();
-		String qrcodeFilePath = TZ_ACTCODE_REPATCH + orgid.toLowerCase();
-		//String dirRealPath = "F:\\qhjg_2\\tzgd_s_java_com\\src\\mainwebapp"+qrcodeFilePath;
-		String dirRealPath = TZ_ACTCODE_TUREPATCH+qrcodeFilePath;
-		File dir = new File(dirRealPath);
+		String webPath = tzGDObject.getWebAppRootPath();
+		String accessPath = "statics" + File.separator + "download" + File.separator + "events" + File.separator + "qrcode";
+		
+		String filePath = webPath + accessPath;
+		File dir = new File(filePath);
 		if (!dir.exists()) {
 			dir.mkdirs();
 		}
 		
-		qrcodeFilePath = dirRealPath + "/" + fileName;
-				
-		String fileRealPath = qrcodeFilePath;
+		String qrcodeFilePath = filePath + File.separator + fileName;
 
-		Map<EncodeHintType, ErrorCorrectionLevel> hintMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
-
+//		Map<EncodeHintType, ErrorCorrectionLevel> hintMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
+//		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+		
+		Map<EncodeHintType, Object> hintMap = new HashMap<EncodeHintType, Object>();
+		//设置白边
+		hintMap.put(EncodeHintType.MARGIN, 1);
+		//指定编码格式    
+		hintMap.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+		// 指定纠错等级    
 		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
 
 		try {
-			QRCode.createQRCode(qrCodeInfo, fileRealPath, charSet, hintMap, qrCodeWidth, qrCodeHeight);
+//			QRCode.createQRCode(qrCodeInfo, qrcodeFilePath, charSet, hintMap, qrCodeWidth, qrCodeHeight);
+			QRCode.createQRCode2(qrCodeInfo, qrcodeFilePath, charSet, hintMap, qrCodeWidth, qrCodeHeight);
 			
-			strReturn=TZ_COMTENTURL+TZ_ACTCODE_REPATCH+orgid.toLowerCase()+"/"+fileName;
+			strReturn = File.separator + accessPath + File.separator + fileName;
 			
 		} catch (WriterException e) {
 			e.printStackTrace();
