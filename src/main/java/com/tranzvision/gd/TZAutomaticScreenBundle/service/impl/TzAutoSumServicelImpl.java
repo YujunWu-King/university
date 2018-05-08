2870ea0af97c74af8079ead9f24e12048ff551ce
caoyang <hannibal005@163.com>
2018-05-08 16:05:19 +0800

    自动计算分数

--- src/main/java/com/tranzvision/gd/TZAutomaticScreenBundle/service/impl/TzAutoSumServicelImpl.java
+++ src/main/java/com/tranzvision/gd/TZAutomaticScreenBundle/service/impl/TzAutoSumServicelImpl.java
@@ -1,15 +1,18 @@
 package com.tranzvision.gd.TZAutomaticScreenBundle.service.impl;
 
 import java.math.BigDecimal;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tranzvision.gd.TZAuthBundle.service.impl.TzLoginServiceImpl;
 import com.tranzvision.gd.TZAutomaticScreenBundle.dao.PsTzCjxTblMapper;
-import com.tranzvision.gd.TZAutomaticScreenBundle.dao.PsTzCsKsTblMapper;
-import com.tranzvision.gd.TZAutomaticScreenBundle.dao.PsTzSrmbaInsTblMapper;
 import com.tranzvision.gd.TZAutomaticScreenBundle.model.PsTzCjxTblKey;
 import com.tranzvision.gd.TZAutomaticScreenBundle.model.PsTzCjxTblWithBLOBs;
 import com.tranzvision.gd.TZAutomaticScreenBundle.model.PsTzCsKsTbl;
@@ -17,6 +20,8 @@ import com.tranzvision.gd.TZAutomaticScreenBundle.model.PsTzCsKsTblKey;
 import com.tranzvision.gd.TZBaseBundle.service.impl.FrameworkImpl;
 import com.tranzvision.gd.TZClassSetBundle.dao.PsTzClassInfTMapper;
 import com.tranzvision.gd.TZClassSetBundle.model.PsTzClassInfT;
+import com.tranzvision.gd.TZMbaPwClpsBundle.dao.PsTzMsPskshTblMapper;
+import com.tranzvision.gd.TZMbaPwClpsBundle.model.PsTzMsPskshTbl;
 import com.tranzvision.gd.util.base.JacksonUtil;
 import com.tranzvision.gd.util.sql.SqlQuery;
 import com.tranzvision.gd.util.sql.TZGDObject;
@@ -46,7 +51,13 @@ public class TzAutoSumServicelImpl extends FrameworkImpl {
 	private PsTzCjxTblMapper psTzCjxTblMapper;
 
 	@Autowired
-	private PsTzCsKsTblMapper psTzCsKsTblMapper;
+	private TzLoginServiceImpl tzLoginServiceImpl;
+
+	@Autowired
+	private HttpServletRequest request;
+
+	@Autowired
+	private PsTzMsPskshTblMapper psTzMsPskshTblMapper;
 
 	@Override
 	public String tzOther(String strType, String strParams, String[] errorMsg) {
@@ -56,12 +67,148 @@ public class TzAutoSumServicelImpl extends FrameworkImpl {
 			case "RunSum": // 计算总分
 				strRet = this.tzRunSumProcess(strParams, errorMsg);
 				break;
+			case "RunMSSum": // 计算面试总分
+				strRet = this.tzRunMSSumProcess(strParams, errorMsg);
+				break;
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			errorMsg[0] = "1";
 			errorMsg[1] = "操作异常。" + e.getMessage();
 		}
+		return strRet;
+	}
+
+	/**
+	 * 计算面试分数
+	 * 
+	 * @param strParams
+	 * @param errorMsg
+	 * @return
+	 */
+	private String tzRunMSSumProcess(String strParams, String[] errorMsg) {
+		String strRet = "";
+		Map<String, Object> rtnMap = new HashMap<String, Object>();
+		rtnMap.put("status", "");
+		rtnMap.put("msg", "");
+
+		JacksonUtil jacksonUtil = new JacksonUtil();
+		try {
+			jacksonUtil.json2Map(strParams);
+			// 班级ID
+			String classId = jacksonUtil.getString("classId");
+			String batchId = jacksonUtil.getString("batchId");
+
+			if (!"".equals(classId) && classId != null && !"".equals(batchId) && batchId != null) {
+				// 由于是非AE程序，为了效率 ，大部分数据库操作 全部放在循环外面执行
+				PsTzClassInfT psTzClassInfT = psTzClassInfTMapper.selectByPrimaryKey(classId);
+
+				// 面试自动模型ID
+				String socreModelId = psTzClassInfT.getTzMscjScorMdId();
+				String oprId = tzLoginServiceImpl.getLoginedManagerOprid(request);
+				long appInsId = 0;
+				Long LscoreInsId = null;
+				long scoreInsId = 0;
+				String TZ_SCORE_ITEM_ID = "";
+				String TZ_SCORE_NUM = "";
+
+				// 自动打分总分
+
+				PsTzMsPskshTbl psTzMsPskshTbl = null;
+				PsTzCjxTblWithBLOBs psTzCjxTblWithBLOBs = null;
+				PsTzCjxTblKey psTzCjxTblKey = null;
+
+				// 班级批次下参与自动初筛的考生 为该班级下 报名表提交并且审核通过的考生
+				String sql = "select DISTINCT TZ_APP_INS_ID from PS_TZ_MP_PW_KS_TBL A where TZ_CLASS_ID = ? AND TZ_APPLY_PC_ID = ?";
+
+				String avgsql = tzSQLObject.getSQLText("SQL.TZAutomaticScreenBundle.GetMsAvg");
+				List<Map<String, Object>> scoreAvgList = null;
+
+				List<Map<String, Object>> appInsList = sqlQuery.queryForList(sql, new Object[] { classId, batchId });
+				boolean exists;
+				for (Map<String, Object> appInsMap : appInsList) {
+					appInsId = Long.valueOf(appInsMap.get("TZ_APP_INS_ID").toString());
+
+					scoreAvgList = sqlQuery.queryForList(avgsql,
+							new Object[] { classId, batchId, String.valueOf(appInsId) });
+
+					if (scoreAvgList.size() > 0) {
+						sql = "select TZ_SCORE_INS_ID from PS_TZ_MSPS_KSH_TBL where TZ_CLASS_ID = ? AND TZ_APPLY_PC_ID = ? and TZ_APP_INS_ID=?";
+
+						LscoreInsId = sqlQuery.queryForObject(sql,
+								new Object[] { classId, batchId, String.valueOf(appInsId) }, "Long");
+						if (LscoreInsId == null || LscoreInsId.longValue() <= 0) {
+							exists = false;
+							scoreInsId = tzAutoScreenEngineServiceImpl.creatNewScoreInstanceId(socreModelId, oprId);
+							psTzMsPskshTbl = new PsTzMsPskshTbl();
+							psTzMsPskshTbl.setTzAppInsId(appInsId);
+							psTzMsPskshTbl.setTzApplyPcId(batchId);
+							psTzMsPskshTbl.setTzClassId(classId);
+							psTzMsPskshTbl.setTzScoreInsId(scoreInsId);
+							psTzMsPskshTbl.setRowLastmantDttm(new Date());
+							psTzMsPskshTbl.setRowLastmantOprid(oprId);
+							psTzMsPskshTblMapper.updateByPrimaryKeySelective(psTzMsPskshTbl);
+
+						} else {
+							exists = true;
+							scoreInsId = LscoreInsId.longValue();
+						}
+						for (Map<String, Object> avgMap : scoreAvgList) {
+							TZ_SCORE_ITEM_ID = avgMap.get("TZ_APP_INS_ID").toString();
+							TZ_SCORE_NUM = avgMap.get("TZ_SCORE_NUM").toString();
+							if (!exists) {
+								// 插入表TZ_CJX_TBL
+								psTzCjxTblWithBLOBs = new PsTzCjxTblWithBLOBs();
+								// 成绩单ID
+								psTzCjxTblWithBLOBs.setTzScoreInsId(new Long(scoreInsId));
+								// 成绩项ID
+								psTzCjxTblWithBLOBs.setTzScoreItemId(TZ_SCORE_ITEM_ID);
+								// 分值
+								psTzCjxTblWithBLOBs.setTzScoreNum(new BigDecimal(TZ_SCORE_NUM));
+								// 插入
+								psTzCjxTblMapper.insert(psTzCjxTblWithBLOBs);
+							} else {
+								psTzCjxTblKey = new PsTzCjxTblKey();
+								psTzCjxTblKey.setTzScoreInsId(new Long(scoreInsId));
+								psTzCjxTblKey.setTzScoreItemId(TZ_SCORE_ITEM_ID);
+								psTzCjxTblWithBLOBs = psTzCjxTblMapper.selectByPrimaryKey(psTzCjxTblKey);
+								if (psTzCjxTblWithBLOBs != null) {
+									// 修改表
+									psTzCjxTblWithBLOBs = new PsTzCjxTblWithBLOBs();
+									psTzCjxTblWithBLOBs.setTzScoreInsId(new Long(scoreInsId));
+									// 成绩项ID
+									psTzCjxTblWithBLOBs.setTzScoreItemId(TZ_SCORE_ITEM_ID);
+									psTzCjxTblWithBLOBs.setTzScoreNum(new BigDecimal(TZ_SCORE_NUM));
+									psTzCjxTblMapper.updateByPrimaryKeySelective(psTzCjxTblWithBLOBs);
+								} else {
+									// 插入表TZ_CJX_TBL
+									psTzCjxTblWithBLOBs = new PsTzCjxTblWithBLOBs();
+									// 成绩单ID
+									psTzCjxTblWithBLOBs.setTzScoreInsId(new Long(scoreInsId));
+									// 成绩项ID
+									psTzCjxTblWithBLOBs.setTzScoreItemId(TZ_SCORE_ITEM_ID);
+									// 分值
+									psTzCjxTblWithBLOBs.setTzScoreNum(new BigDecimal(TZ_SCORE_NUM));
+									// 插入
+									psTzCjxTblMapper.insert(psTzCjxTblWithBLOBs);
+								}
+							}
+						}
+					}
+				}
+				rtnMap.put("status", "0");
+				rtnMap.put("msg", "打分成功");
+			} else {
+				rtnMap.put("status", "-1");
+				rtnMap.put("msg", "参数错误");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			errorMsg[0] = "1";
+			errorMsg[1] = "操作异常。" + e.getMessage();
+		}
+
+		strRet = jacksonUtil.Map2json(rtnMap);
 		return strRet;
 	}
 
@@ -94,46 +241,61 @@ public class TzAutoSumServicelImpl extends FrameworkImpl {
 				// 面试自动模型ID
 				String msModelId = psTzClassInfT.getTzMscjScorMdId();
 
+				String oprId = tzLoginServiceImpl.getLoginedManagerOprid(request);
+
 				String orgId = psTzClassInfT.getTzJgId();
 
-				// 自动初筛 报名表实例ID和成绩单ID对应表
-				Map<String, String> autoCSKS = new HashMap<String, String>();
-
-				String sql = "select TZ_APP_INS_ID,TZ_SCORE_INS_ID from PS_TZ_CS_KS_TBL where TZ_CLASS_ID=? and TZ_APPLY_PC_ID=?";
-				List<Map<String, Object>> list = sqlQuery.queryForList(sql, new Object[] { classId, batchId });
-				for (Map<String, Object> map : list) {
-					autoCSKS.put(map.get("TZ_APP_INS_ID").toString(),
-							map.get("TZ_SCORE_INS_ID") == null ? "" : map.get("TZ_SCORE_INS_ID").toString());
-				}
-
 				// 自动初筛成绩模型里面配置的，没有自动初筛执行类的成绩项目(也就是留给管理员打分的项目)
-				sql = tzSQLObject.getSQLText("SQL.TZAutomaticScreenBundle.TzModelTreeRootNode");
+				String sql = tzSQLObject.getSQLText("SQL.TZAutomaticScreenBundle.TzModelTreeRootNode");
 				Map<String, Object> autoRootMap = sqlQuery.queryForMap(sql, new Object[] { socreModelId, orgId });
 				Map<String, Object> msRootMap = sqlQuery.queryForMap(sql, new Object[] { msModelId, orgId });
-				
-				
-				//面试   报名表实例ID和成绩单ID对应表
-				Map<String, String> msCSKS = new HashMap<String, String>();
 
-				sql = "select TZ_APP_INS_ID,TZ_SCORE_INS_ID from PS_TZ_CS_KS_TBL where TZ_CLASS_ID=? and TZ_APPLY_PC_ID=?";
-				list = sqlQuery.queryForList(sql, new Object[] { classId, batchId });
+				// 自动初筛 报名表实例ID和总成绩
+				Map<String, String> autoCSKS = new HashMap<String, String>();
+				sql = "SELECT A.TZ_APP_INS_ID,B.TZ_SCORE_NUM FROM PS_TZ_CS_KS_TBL A,PS_TZ_CJX_TBL B WHERE A.TZ_CLASS_ID =? AND A.TZ_APPLY_PC_ID =? AND A.TZ_SCORE_INS_ID = B.TZ_SCORE_INS_ID AND B.TZ_SCORE_ITEM_ID =?";
+				List<Map<String, Object>> list = sqlQuery.queryForList(sql,
+						new Object[] { classId, batchId, autoRootMap.get("TREE_NODE").toString() });
 				for (Map<String, Object> map : list) {
 					autoCSKS.put(map.get("TZ_APP_INS_ID").toString(),
-							map.get("TZ_SCORE_INS_ID") == null ? "" : map.get("TZ_SCORE_INS_ID").toString());
+							map.get("TZ_SCORE_NUM") == null ? "" : map.get("TZ_SCORE_NUM").toString());
 				}
-				
+
+				// 面试 报名表实例ID和成绩单ID对应表和总成绩
+				Map<String, String> msCSKS = new HashMap<String, String>();
+				sql = "SELECT A.TZ_APP_INS_ID,B.TZ_SCORE_NUM FROM PS_TZ_MSPS_KSH_TBL A,PS_TZ_CJX_TBL B WHERE A.TZ_CLASS_ID =? AND A.TZ_APPLY_PC_ID =? AND A.TZ_SCORE_INS_ID = B.TZ_SCORE_INS_ID AND B.TZ_SCORE_ITEM_ID =?";
+				list = sqlQuery.queryForList(sql,
+						new Object[] { classId, batchId, msRootMap.get("TREE_NODE").toString() });
+				for (Map<String, Object> map : list) {
+					msCSKS.put(map.get("TZ_APP_INS_ID").toString(),
+							map.get("TZ_SCORE_NUM") == null ? "" : map.get("TZ_SCORE_NUM").toString());
+				}
 
 				long appInsId = 0;
 				long scoreInsId = 0;
-				String treeName = "";
-				String rootNode = "";
+
+				// 评分标准
+				String bzId = sqlQuery.queryForObject(
+						"select TZ_HARDCODE_VAL from PS_TZ_HARDCD_PNT where TZ_HARDCODE_PNT=?",
+						new Object[] { "TZ_MBA_MS_RESULT" }, "String");
+
+				String bzsql = "select TZ_M_FBDZ_MX_SM from PS_TZ_FBDZ_MX_TBL where TZ_M_FBDZ_ID=? and TZ_M_FBDZ_MX_SX>=? and TZ_M_FBDZ_MX_XX<=? ";
+
+				String bzDsc = "";
+
 				PsTzCjxTblKey psTzCjxTblKey = null;
-				PsTzCjxTblWithBLOBs psTzCjxTbl = null;
-				PsTzCsKsTblKey psTzCsKsTblKey = null;
-				PsTzCsKsTbl psTzCsKsTbl = null;
+				PsTzCjxTblWithBLOBs psTzCjxTblWithBLOBs = null;
+				Long LscoreInsId = null;
+				PsTzMsPskshTbl psTzMsPskshTbl = null;
 
 				// 自动打分总分
 				float autoScore = 0;
+				String strAutoScore = "";
+
+				// 面试总分
+				float msScore = 0;
+				String strMsScore = "";
+
+				float sumScore = 0;
 
 				// 班级批次下参与自动初筛的考生 为该班级下 报名表提交并且审核通过的考生
 				sql = "SELECT A.TZ_APP_INS_ID FROM PS_TZ_MSPS_KSH_TBL A where A.TZ_CLASS_ID = ? AND A.TZ_APPLY_PC_ID = ?";
@@ -141,44 +303,86 @@ public class TzAutoSumServicelImpl extends FrameworkImpl {
 				for (Map<String, Object> appInsMap : appInsList) {
 					appInsId = Long.valueOf(appInsMap.get("TZ_APP_INS_ID").toString());
 
-					// 由于管理员打分的成绩模型 放在 自动打分模型里面，所以需要重新计算下自动打分模型的 总分
-					psTzCsKsTblKey = new PsTzCsKsTblKey();
-					psTzCsKsTblKey.setTzClassId(classId);
-					psTzCsKsTblKey.setTzApplyPcId(batchId);
-					psTzCsKsTblKey.setTzAppInsId(appInsId);
-					psTzCsKsTbl = psTzCsKsTblMapper.selectByPrimaryKey(psTzCsKsTblKey);
+					sql = "select TZ_SCORE_INS_ID from PS_TZ_MSPS_KSH_TBL where TZ_CLASS_ID = ? AND TZ_APPLY_PC_ID = ? and TZ_APP_INS_ID=?";
 
-					scoreInsId = psTzCsKsTbl.getTzScoreInsId() == null ? 0 : psTzCsKsTbl.getTzScoreInsId();
-
-					autoScore = 0;
-					if (rootMap != null && scoreInsId > 0) {
-						treeName = rootMap.get("TREE_NAME").toString();
-						int rootNodeNum = Integer.valueOf(rootMap.get("TREE_NODE_NUM").toString());
-						rootNode = rootMap.get("TREE_NODE").toString();
-
-						autoScore = tzAutoScreenEngineServiceImpl.calculateTreeNodeScore(scoreInsId, treeName, orgId,
-								rootNodeNum, rootNode);
-						BigDecimal rootScore = BigDecimal.valueOf(Double.valueOf(Float.toString(autoScore)));
-
-						psTzCjxTblKey = new PsTzCjxTblKey();
-						psTzCjxTblKey.setTzScoreInsId(scoreInsId);
-						psTzCjxTblKey.setTzScoreItemId(rootNode);
-						psTzCjxTbl = psTzCjxTblMapper.selectByPrimaryKey(psTzCjxTblKey);
-						if (psTzCjxTbl != null) {
-							psTzCjxTbl.setTzScoreNum(rootScore);
-							psTzCjxTblMapper.updateByPrimaryKey(psTzCjxTbl);
-						} else {
-							psTzCjxTbl = new PsTzCjxTblWithBLOBs();
-							psTzCjxTbl.setTzScoreInsId(scoreInsId);
-							psTzCjxTbl.setTzScoreItemId(rootNode);
-							psTzCjxTbl.setTzScoreNum(rootScore);
-							psTzCjxTblMapper.insert(psTzCjxTbl);
-						}
+					LscoreInsId = sqlQuery.queryForObject(sql,
+							new Object[] { classId, batchId, String.valueOf(appInsId) }, "Long");
+					if (LscoreInsId == null || LscoreInsId.longValue() <= 0) {
+						scoreInsId = tzAutoScreenEngineServiceImpl.creatNewScoreInstanceId(socreModelId, oprId);
+						psTzMsPskshTbl = new PsTzMsPskshTbl();
+						psTzMsPskshTbl.setTzAppInsId(appInsId);
+						psTzMsPskshTbl.setTzApplyPcId(batchId);
+						psTzMsPskshTbl.setTzClassId(classId);
+						psTzMsPskshTbl.setTzScoreInsId(scoreInsId);
+						psTzMsPskshTbl.setRowLastmantDttm(new Date());
+						psTzMsPskshTbl.setRowLastmantOprid(oprId);
+						psTzMsPskshTblMapper.updateByPrimaryKeySelective(psTzMsPskshTbl);
 
 					} else {
-						// 没有自动初筛
+						scoreInsId = LscoreInsId.longValue();
 					}
+
+					if (autoCSKS.get(String.valueOf(appInsId)) == null) {
+						strAutoScore = "0";
+					} else {
+						strAutoScore = autoCSKS.get(String.valueOf(appInsId));
+					}
+					if (strAutoScore.equals("")) {
+						autoScore = 0;
+					} else {
+						autoScore = Float.parseFloat(strAutoScore);
+					}
+
+					if (msCSKS.get(String.valueOf(appInsId)) == null) {
+						strMsScore = "0";
+					} else {
+						strMsScore = msCSKS.get(String.valueOf(appInsId));
+					}
+					if (strMsScore.equals("")) {
+						msScore = 0;
+					} else {
+						msScore = Float.parseFloat(strMsScore);
+					}
+
+					sumScore = autoScore + msScore;
+					// 计算档次
+					bzDsc = sqlQuery.queryForObject(bzsql,
+							new Object[] { bzId, String.valueOf(sumScore), String.valueOf(sumScore) }, "String");
+
+					psTzCjxTblKey = new PsTzCjxTblKey();
+					psTzCjxTblKey.setTzScoreInsId(new Long(scoreInsId));
+					psTzCjxTblKey.setTzScoreItemId("");
+					psTzCjxTblWithBLOBs = psTzCjxTblMapper.selectByPrimaryKey(psTzCjxTblKey);
+					if (psTzCjxTblWithBLOBs != null) {
+						// 修改表
+						psTzCjxTblWithBLOBs = new PsTzCjxTblWithBLOBs();
+						psTzCjxTblWithBLOBs.setTzScoreInsId(new Long(scoreInsId));
+						// 成绩项ID
+						psTzCjxTblWithBLOBs.setTzScoreItemId("SumTotal");
+						psTzCjxTblWithBLOBs.setTzScoreNum(new BigDecimal(sumScore));
+						psTzCjxTblWithBLOBs.setTzScoreBz(bzDsc);
+						psTzCjxTblWithBLOBs.setTzScoreDfgc(
+								"自动打分/管理员打分" + String.valueOf(autoScore) + "分|面试打分" + String.valueOf(msScore) + "分");
+						psTzCjxTblMapper.updateByPrimaryKeySelective(psTzCjxTblWithBLOBs);
+					} else {
+						// 插入表TZ_CJX_TBL
+						psTzCjxTblWithBLOBs = new PsTzCjxTblWithBLOBs();
+						// 成绩单ID
+						psTzCjxTblWithBLOBs.setTzScoreInsId(new Long(scoreInsId));
+						// 成绩项ID
+						psTzCjxTblWithBLOBs.setTzScoreItemId("SumTotal");
+						// 分值
+						psTzCjxTblWithBLOBs.setTzScoreNum(new BigDecimal(sumScore));
+						psTzCjxTblWithBLOBs.setTzScoreBz(bzDsc);
+						psTzCjxTblWithBLOBs.setTzScoreDfgc(
+								"自动打分/管理员打分" + String.valueOf(autoScore) + "分|面试打分" + String.valueOf(msScore) + "分");
+						// 插入
+						psTzCjxTblMapper.insert(psTzCjxTblWithBLOBs);
+					}
+
 				}
+				rtnMap.put("status", "0");
+				rtnMap.put("msg", "打分成功");
 
 			}
 		} catch (Exception e) {
