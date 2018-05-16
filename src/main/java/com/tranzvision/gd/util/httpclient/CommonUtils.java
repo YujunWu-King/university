211a7e3c49b120c5958bb74b8b291ad360fa4251
caoyang <hannibal005@163.com>
2018-05-16 19:01:44 +0800

    小米PAD 显示报名表

--- src/main/java/com/tranzvision/gd/util/httpclient/CommonUtils.java
+++ src/main/java/com/tranzvision/gd/util/httpclient/CommonUtils.java
@@ -30,11 +30,23 @@ public class CommonUtils {
 
 		System.out.println("sUserAgent:" + sUserAgent);
 
+		// sUserAgent:Mozilla/5.0 (Linux; U; Android 4.4.4; zh-cn; MI PAD
+		// Build/KTU84P) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0
+		// Chrome/53.0.2785.146 Mobile Safari/537.36 XiaoMi/MiuiBrowser/9.3.2
+		
+		// sUserAgent:Mozilla/5.0 (Linux; Android 8.0.0; MIX 2
+		// Build/OPR1.170623.027; wv) AppleWebKit/537.36 (KHTML, like Gecko)
+		// Version/4.0 Chrome/62.0.3202.84 Mobile Safari/537.36
+		// MicroMessenger/6.6.6.1300(0x26060637) NetType/WIFI Language/zh_CN
 		String[] mobileAgents = { "Windows CE", "iPod", "Symbian", "iPhone", "BlackBerry", "Android", "Windows Phone" };
 		if (sUserAgent.indexOf("Android") > -1 && (sUserAgent.indexOf("ERD79) > -1 || sUserAgent.indexOf('MZ60") > -1
 				|| sUserAgent.indexOf("GT-P7") > -1 || sUserAgent.indexOf("SCH-P7") > -1)) {
 		} else {
 			for (int i = 0; i < mobileAgents.length; i++) {
+				if (sUserAgent.indexOf("XiaoMi") > -1) {
+					break;
+				}
+
 				if (sUserAgent.indexOf(mobileAgents[i]) > -1) {
 					isMobile = true;
 					break;
